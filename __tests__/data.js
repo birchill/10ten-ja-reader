da581dd3c488532caddc798b67bbd8f6dc8b4dfd
Brian Birtles <birtles@gmail.com>
2017-08-20 16:56:50 +0900

    Replace fetch mock module with a local mock

--- __tests__/data.js
+++ __tests__/data.js
@@ -1,16 +1,25 @@
+const fs = require('fs');
+
 const Dictionary = require('../data');
 
 // Mock browser.extension.getURL
 global.browser = { extension: { getURL: jest.fn(url => url) } };
 
+// Mock fetch
+window.fetch = jest.fn().mockImplementation(url =>
+  new Promise((resolve, reject) => {
+    fs.readFile(`${__dirname}/../${url}`, function (err, data) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve({ text: () => data.toString() });
+    });
+  })
+);
+
 describe('Dictionary', () => {
-  beforeEach(() => {
-    fetch.resetMocks();
-  });
-
   it('loads', async () => {
-    fetch.mockResponse('× [ばつ] /(n,uk) x-mark (used to indicate an incorrect answer in a test, etc.)/impossibility/futility/uselessness/');
-
     const dict = new Dictionary(true /* doNames */);
 
     await dict.loaded;
