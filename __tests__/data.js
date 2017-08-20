34c6ccfd538494f22caa7ee3658bbd77eb3fce71
Brian Birtles <birtles@gmail.com>
2017-08-20 17:09:41 +0900

    Start filling out test plan

--- __tests__/data.js
+++ __tests__/data.js
@@ -18,11 +18,31 @@ window.fetch = jest.fn().mockImplementation(url =>
   })
 );
 
+// Common dictionary instance to save loading each time
+const sharedDict = new Dictionary(true /* doNames */);
+
 describe('Dictionary', () => {
   it('loads', async () => {
-    const dict = new Dictionary(true /* doNames */);
-
-    await dict.loaded;
+    await sharedDict.loaded;
     // Nothing to check
   });
+
+  it('finds an exact match', async () => {
+    const result = await sharedDict.wordSearch('蛋白質');
+    console.log(result);
+  });
+
+  // TODO: Test that variant spelling with katakana matches
+  // TODO: Test that variant spelling with hiragana matches
+  // TODO: Test that variant spelling with all hiragana matches
+  // TODO: Test that component also matches
+  // TODO: Find a match with trailing characters
+  // TODO: Test input normalization
+  // TODO: Test de-inflection
+  // TODO: Test names dictionary handling
+  //       e.g. create a new Dictionary specifying 'false' for doNames but then
+  //       do a work lookup where 'doNames' is true.
+  //       e.g.(2) do a work look up where 'doNames' is true and check we
+  //       *don't* match names
+  // TODO: Test kanji lookup
 });
