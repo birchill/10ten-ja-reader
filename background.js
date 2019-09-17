1db666b072d44bea4d5161b2ea516cb9ba7aa3bb
Deshaun Crawford <deshaun_crawford@apple.com>
2019-09-16 23:38:00 -0700

    Code clean up. Initial changes for preserving local storage settings.

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
+var optionsList = [
+	"copySeparator",
+	"disablekeys",
+	"highlight",
+	"kanjicomponents",
+	"lineEnding",
+	"maxClipCopyEntries",
+	"minihelp",
+	"onlyreading",
+	"popupcolor",
+	"popupDelay",
+	"textboxhl",
+	"showOnKey"];
+
 chrome.browserAction.onClicked.addListener(rcxMain.inlineToggle);
 chrome.tabs.onSelectionChanged.addListener(rcxMain.onTabSelect);
 chrome.runtime.onMessage.addListener(
@@ -12,10 +26,10 @@ chrome.runtime.onMessage.addListener(
 				var e = rcxMain.search(request.text, request.dictOption);
 				response(e);
 				break;
-		/*  case 'nextDict':
-							console.log('nextDict');
-							rcxMain.nextDict();
-							break;*/
+			/*  case 'nextDict':
+								console.log('nextDict');
+								rcxMain.nextDict();
+								break;*/
 			case 'resetDict':
 				console.log('resetDict');
 				rcxMain.resetDict();
@@ -46,87 +60,304 @@ chrome.runtime.onMessage.addListener(
 				console.log(request);
 		}
 	})
-var getItems = ["popupcolor", "highlight", "textboxhl", "onlyreading",
-	"minihelp", "disablekeys", "kanjicomponents", "lineEnding", "copySeparator",
-	"maxClipCopyEntries", "popupDelay",
-	"showOnKey"];
-chrome.storage.sync.get(getItems,
+
+chrome.storage.sync.get(optionsList,
 	function (items) {
-		if (initStorage("v0.8.92", true)) {
-			// v0.7
-			initStorage("popupcolor", "blue");
-			initStorage("highlight", true);
-
-			// v0.8
-			// No changes to options
-
-			// V0.8.5
-			initStorage("textboxhl", false);
-
-			// v0.8.6
-			initStorage("onlyreading", false);
-			// v0.8.8
-			if (items.highlight == "yes")
-				items.highlight = "true";
-			if (items.highlight == "no")
-				items.highlight = "false";
-			if (items.textboxhl == "yes")
-				items.textboxhl = "true";
-			if (items.textboxhl == "no")
-				items.textboxhl = "false";
-			if (items.onlyreading == "yes")
-				items.onlyreading = "true";
-			if (items.onlyreading == "no")
-				items.onlyreading = "false";
-			initStorage("copySeparator", "tab");
-			initStorage("maxClipCopyEntries", "7");
-			initStorage("lineEnding", "n");
-			initStorage("minihelp", "true");
-			initStorage("disablekeys", "false");
-			initStorage("kanjicomponents", "true");
-
-			for (i = 0; i * 2 < rcxDict.prototype.kanjiInfoLabelList.length; i++) {
-				initStorage(rcxDict.prototype.kanjiInfoLabelList[i * 2], "true")
-			}
-
-			// v0.8.92
-			initStorage("popupDelay", "150");
-			initStorage("showOnKey", "");
-		}
-
-		/**
-		 * Initializes the localStorage for the given key.
-		 * If the given key is already initialized, nothing happens.
-		 *
-		 * @author Teo (GD API Guru)
-		 * @param key The key for which to initialize
-		 * @param initialValue Initial value of localStorage on the given key
-		 * @return boolean if a value is assigned or false if nothing happens
-		 */
-		function initStorage(key, initialValue) {
-			var currentValue = localStorage[key];
-			if (!currentValue) {
-				localStorage[key] = initialValue;
-				return true;
-			}
-			return false;
-		}
-
 		rcxMain.config = {};
-		rcxMain.config.css = items.popupcolor;
-		rcxMain.config.highlight = items.highlight;
-		rcxMain.config.textboxhl = items.textboxhl;
-		rcxMain.config.onlyreading = items.onlyreading;
-		rcxMain.config.copySeparator = items.copySeparator;
-		rcxMain.config.maxClipCopyEntries = items.maxClipCopyEntries;
-		rcxMain.config.lineEnding = items.lineEnding;
-		rcxMain.config.minihelp = items.minihelp;
-		rcxMain.config.popupDelay = items.popupDelay;
-		rcxMain.config.disablekeys = items.disablekeys;
-		rcxMain.config.showOnKey = items.showOnKey;
-		rcxMain.config.kanjicomponents = items.kanjicomponents;
-		// rcxMain.config.kanjiinfo = new Array(rcxDict.prototype.kanjiInfoLabelList.length / 2);
-		// for (i = 0; i * 2 < rcxDict.prototype.kanjiInfoLabelList.length; i++) {
-		//     rcxMain.config.kanjiinfo[i] = localStorage[rcxDict.prototype.kanjiInfoLabelList[i * 2]];
+		fillRcxValsFromLocalStore(items)
+		saveOptionsToCloudStorage()
+		//Todo: Erase after completely moving to fillRcxValsFromLocalStore
+		// rcxMain.config.css = items.popupcolor;
+		// rcxMain.config.highlight = items.highlight;
+		// rcxMain.config.textboxhl = items.textboxhl;
+		// rcxMain.config.onlyreading = items.onlyreading;
+		// rcxMain.config.copySeparator = items.copySeparator;
+		// rcxMain.config.maxClipCopyEntries = items.maxClipCopyEntries;
+		// rcxMain.config.lineEnding = items.lineEnding;
+		// rcxMain.config.minihelp = items.minihelp;
+		// rcxMain.config.popupDelay = items.popupDelay;
+		// rcxMain.config.disablekeys = items.disablekeys;
+		// rcxMain.config.showOnKey = items.showOnKey;
+		// rcxMain.config.kanjicomponents = items.kanjicomponents;
+		//
+		//
+		// for (i = 0; i*2 < rcxDict.prototype.kanjiInfoLabelList.length; i++) {
+		// 	rcxMain.config.kanjiinfo[i] = items.kanjiinfo[i*2];
 		// }
-	})
+
+
+		// for (i = 0; i * 2 < rcxDict.prototype.kanjiInfoLabelList.length; i++) {
+		// 	initStorage(rcxDict.prototype.kanjiInfoLabelList[i * 2], "true")
+		// }
+
+		// Todo: Migrate this version logic and version history to - > fillRcxValsFromLocalStore()
+		// if (initStorage("v0.8.92", true)) {
+		// 	// v0.7
+		// 	initStorage("popupcolor", "blue");
+		// 	initStorage("highlight", true);
+		//
+		// 	// v0.8
+		// 	// No changes to options
+		//
+		// 	// V0.8.5
+		// 	initStorage("textboxhl", false);
+		//
+		// 	// v0.8.6
+		// 	initStorage("onlyreading", false);
+		// 	// v0.8.8
+		// 	if (items.highlight == "yes")
+		// 		items.highlight = "true";
+		// 	if (items.highlight == "no")
+		// 		items.highlight = "false";
+		// 	if (items.textboxhl == "yes")
+		// 		items.textboxhl = "true";
+		// 	if (items.textboxhl == "no")
+		// 		items.textboxhl = "false";
+		// 	if (items.onlyreading == "yes")
+		// 		items.onlyreading = "true";
+		// 	if (items.onlyreading == "no")
+		// 		items.onlyreading = "false";
+		// 	initStorage("copySeparator", "tab");
+		// 	initStorage("maxClipCopyEntries", "7");
+		// 	initStorage("lineEnding", "n");
+		// 	initStorage("minihelp", "true");
+		// 	initStorage("disablekeys", "false");
+		// 	initStorage("kanjicomponents", "true");
+		//
+		// 	// v0.8.92
+		// 	initStorage("popupDelay", "150");
+		// 	initStorage("showOnKey", "");
+		// }
+
+	});
+
+/**
+ * Initializes the localStorage for the given key.
+ * If the given key is already initialized, nothing happens.
+ *
+ * @author Teo (GD API Guru)
+ * @param key The key for which to initialize
+ * @param initialValue Initial value of localStorage on the given key
+ * @return boolean if a value is assigned or false if nothing happens
+ */
+function saveOptionsToCloudStorage() {
+	chrome.storage.sync.set({
+		// Saving General options
+		"copySeparator": rcxMain.config.copySeparator,
+		"disablekeys": rcxMain.config.disablekeys,
+		"highlight": rcxMain.config.highlight,
+		"kanjicomponents": rcxMain.config.kanjicomponents,
+		//Todo: Package contents of Kanji info object to store in cloud
+		"kanjiInfo":kanjiInfoObject,
+		"lineEnding": rcxMain.config.lineEnding,
+		"maxClipCopyEntries": rcxMain.config.maxClipCopyEntries,
+		"minihelp": rcxMain.config.minihelp,
+		"onlyreading": rcxMain.config.onlyreading,
+		"popupcolor": rcxMain.config.popupcolor,
+		"popupDelay": rcxMain.config.popupDelay,
+		"popupLocation":rcxMain.config.popupLocation,
+		"textboxhl": rcxMain.config.textboxhl,
+
+		// Saving Copy to Clipboard settings
+		"showOnKey":rcxMain.config.showOnKey
+	});
+
+}
+
+/**
+ * Initializes the localStorage for the given key.
+ * If the given key is already initialized, nothing happens.
+ *
+ * @author Teo (GD API Guru)
+ * @param key The key for which to initialize
+ * @param initialValue Initial value of localStorage on the given key
+ * @return true if a value is assigned or false if nothing happens
+ */
+function fillRcxValsFromLocalStore(items) {
+	/** Set Copy Separator Option value */
+	if(items.copySeparator){
+		rcxMain.config.copySeparator =items.copySeparator;
+	}
+
+	if(localStorage["copySeparator"]){
+		rcxMain.config.copySeparator = localStorage["copySeparator"];
+	}
+
+	if(!rcxMain.config.copySeparator){
+		rcxMain.config.copySeparator =  "tab"
+	}
+
+
+	/** Set Disable Keys option value */
+	if(items.disablekeys){
+		rcxMain.config.disablekeys = items.disablekeys;
+	}
+
+
+	if(localStorage["disablekeys"]){
+		rcxMain.config.disablekeys = localStorage["disablekeys"];
+	}
+
+	if(!rcxMain.config.disablekeys)
+		rcxMain.config.disablekeys = "false"
+	}
+
+	/** Set highlight option value */
+	if(items.highlight){
+		rcxMain.config.highlight = items.highlight;
+	}
+
+	if(localStorage["highlight"]){
+		rcxMain.config.highlight = localStorage["highlight"];
+	}
+
+	if(!rcxMain.config.highlight){
+		rcxMain.config.highlight = true
+	}
+
+	/** Set kanji components option value */
+	if(items.kanjicomponents){
+		rcxMain.config.kanjicomponents = items.kanjicomponents;
+	}
+
+	if(localStorage["kanjicomponents"]){
+		rcxMain.config.kanjicomponents = localStorage["kanjicomponents"];
+	}
+
+	if(!rcxMain.config.kanjicomponents){
+		rcxMain.config.kanjicomponents = "true"
+	}
+
+	/** Set lineEnding option value */
+	if(items.lineEnding){
+		rcxMain.config.lineEnding = items.lineEnding;
+	}
+
+	if(localStorage["lineEnding"]){
+		rcxMain.config.lineEnding = localStorage["lineEnding"];
+	}
+
+	if(!rcxMain.config.lineEnding){
+		rcxMain.config.lineEnding = "n"
+	}
+
+	/** Set maxClipCopyEntries option value */
+	if(items.maxClipCopyEntries){
+		rcxMain.config.maxClipCopyEntries = items.maxClipCopyEntries;
+	}
+
+	if(localStorage["maxClipCopyEntries"]){
+		rcxMain.config.maxClipCopyEntries = localStorage["maxClipCopyEntries"];
+	}
+
+	if(!rcxMain.config.maxClipCopyEntries){
+		rcxMain.config.maxClipCopyEntries = "7"
+	}
+
+	/** Set minihelp option value */
+	if(items.minihelp){
+		rcxMain.config.minihelp = items.minihelp;
+	}
+
+	if(localStorage["minihelp"]){
+		rcxMain.config.minihelp = localStorage["minihelp"];
+	}
+
+	if(!rcxMain.config.minihelp){
+		rcxMain.config.minihelp = "true"
+	}
+
+	/** Set onlyreading option value */
+	if(items.onlyreading){
+		rcxMain.config.onlyreading = items.onlyreading;
+	}
+
+	if(localStorage["onlyreading"]){
+		rcxMain.config.onlyreading = localStorage["onlyreading"];
+	}
+
+	if(!rcxMain.config.onlyreading){
+		rcxMain.config.onlyreading = false
+	}
+
+	/** Set popupcolor option value */
+	if(items.popupcolor){
+		rcxMain.config.popupcolor = items.popupcolor;
+	}
+
+	if(localStorage["popupcolor"]){
+		rcxMain.config.popupcolor = localStorage["popupcolor"];
+	}
+
+	if(!rcxMain.config.popupcolor){
+		rcxMain.config.popupcolor = "blue"
+	}
+
+	/** Set popupDelay option value */
+	if(items.popupDelay){
+		rcxMain.config.popupDelay = items.popupDelay;
+	}
+
+	if(localStorage["popupDelay"]){
+		rcxMain.config.popupDelay = localStorage["popupDelay"];
+	}
+
+	if(!rcxMain.config.popupDelay){
+		rcxMain.config.popupDelay = "150"
+	}
+
+	/** Set popupLocation option value */
+	if(items.popupLocation){
+		rcxMain.config.popupLocation = items.popupLocation;
+	}
+
+	if(localStorage["popupLocation"]){
+		rcxMain.config.popupLocation = localStorage["popupLocation"];
+	}
+
+	//Todo: Find popuplocation default value
+	// if(!rcxMain.config.popupLocation){
+	// 	rcxMain.config.popupLocation = "150"
+	// }
+
+	/** Set showOnKey option value */
+	if(items.showOnKey){
+		rcxMain.config.showOnKey = items.showOnKey;
+	}
+
+	if(localStorage["showOnKey"]){
+		rcxMain.config.showOnKey = localStorage["showOnKey"];
+	}
+
+	if(!rcxMain.config.showOnKey){
+		rcxMain.config.showOnKey = ""
+	}
+
+	/** Set textboxhl option value */
+	if(items.textboxhl){
+		rcxMain.config.textboxhl = items.textboxhl;
+	}
+
+	if(localStorage["textboxhl"]){
+		rcxMain.config.textboxhl = localStorage["textboxhl"];
+	}
+
+	if(!rcxMain.config.textboxhl){
+		rcxMain.config.textboxhl = false
+	}
+
+	/** Set kanjiinfo option values */
+	rcxMain.config.kanjiinfo = new Array(rcxDict.prototype.kanjiInfoLabelList.length/2);
+	for (i = 0; i*2 < rcxDict.prototype.kanjiInfoLabelList.length; i++) {
+		if(items.kanjiinfo[i*2]){
+			rcxMain.config.kanjiinfo[i] = items.kanjiinfo[i*2];
+		}
+
+		if(localStorage[rcxDict.prototype.kanjiInfoLabelList[i*2]]) {
+			rcxMain.config.kanjiinfo[i] = localStorage[rcxDict.prototype.kanjiInfoLabelList[i * 2]];
+		}
+
+		if(!rcxMain.config.kanjiinfo){
+			rcxMain.config.kanjiinfo[i] = "true"
+		}
+	}
--- options.js
+++ options.js
@@ -1,15 +1,5 @@
-
-//Todo: Update config
-//TOdo: add the second tobi addition
-// Get the current options.
 function get_options() {
-	//Todo: Order these
-
-	var getItems = ["popupcolor","highlight","textboxhl","onlyreading",
-		"minihelp","disablekeys", "kanjicomponents","lineEnding", "copySeparator",
-		"maxClipCopyEntries","popupDelay",
-		"showOnKey","popupLocation","kanjiInfo"];
-	chrome.storage.sync.get(getItems,
+	chrome.storage.sync.get(optionsList,
 		function(items) {
 
 			for(var i=0; i < document.optform.popupcolor.length; ++i) {
@@ -28,7 +18,6 @@ function get_options() {
 			document.optform.kanjicomponents.checked = items.kanjicomponents;
 			document.optform.popupDelay.value = items.popupDelay;
 
-
 			kanjiInfoLabelList = chrome.extension.getBackgroundPage().rcxDict.prototype.kanjiInfoLabelList;
 
 			for (i = 0; i*2 < kanjiInfoLabelList.length; i++) {
@@ -38,108 +27,103 @@ function get_options() {
 				document.getElementById(kanjiInfoLabelList[i*2]).checked = items.kanjiInfo[kanjiInfoLabelList[i*2]] //== 'true' ? true : false;
 			}
 
-	for(var i=0; i < document.optform.lineEnding.length; ++i) {
-		if(document.optform.lineEnding[i].value === items.lineEnding) {
-//Todo: Update config
-//TOdo: add the second tobi addition
-// Get the current options
-		
+			for(var i=0; i < document.optform.lineEnding.length; ++i) {
+				if(document.optform.lineEnding[i].value === items.lineEnding) {
+				// Get the user input
+				function save_options() {
+					//Todo: Add Ordering of variables
+					var popupcolor = document.optform.popupcolor.value;
+					var highlight = document.optform.highlighttext.checked;
+					var textboxhl = document.optform.textboxhl.checked;
+					var onlyreading = document.optform.onlyreading.checked;
+					var minihelp = document.optform.minihelp.checked;
+					var disablekeys = document.optform.disablekeys.checked;
+					var kanjicomponents = document.optform.kanjicomponents.checked;
+					var lineEnding = document.optform.lineEnding.value;
+					var copySeparator = document.optform.copySeparator.value;
+					var maxClipCopyEntries = document.optform.maxClipCopyEntries.value;
+					var popupDelay = getPopUpDelay();
+					var popupLocation =document.optform.popupLocation.value;
+					var showOnKey = document.optform.showOnKey.value;
+					var kanjiInfoObject = {};
+					var kanjiInfoLabelList = chrome.extension.getBackgroundPage().rcxDict.prototype.kanjiInfoLabelList;
+					var kanjiinfoarray = new Array(kanjiInfoLabelList.length/2);
 
-// Get the user input
-			function save_options() {
-				//Todo: Add Ordering of variables
-				var popupcolor = document.optform.popupcolor.value;
-				var highlight = document.optform.highlighttext.checked;
-				var textboxhl = document.optform.textboxhl.checked;
-				var onlyreading = document.optform.onlyreading.checked;
-				var minihelp = document.optform.minihelp.checked;
-				var disablekeys = document.optform.disablekeys.checked;
-				var kanjicomponents = document.optform.kanjicomponents.checked;
-				var lineEnding = document.optform.lineEnding.value;
-				var copySeparator = document.optform.copySeparator.value;
-				var maxClipCopyEntries = document.optform.maxClipCopyEntries.value;
-				var popupDelay = getPopUpDelay();
-				var popupLocation =document.optform.popupLocation.value;
-				var showOnKey = document.optform.showOnKey.value;
-				var kanjiInfoObject = {};
-				var kanjiInfoLabelList = chrome.extension.getBackgroundPage().rcxDict.prototype.kanjiInfoLabelList;
-				var kanjiinfoarray = new Array(kanjiInfoLabelList.length/2);
-
-				// Setting Kanji values
-				for (i = 0; i*2 < kanjiInfoLabelList.length; i++) {
-					kanjiInfoObject[kanjiInfoLabelList[i * 2]] = document.getElementById(kanjiInfoLabelList[i * 2]).checked;
-					kanjiinfoarray[i] = kanjiInfoObject[kanjiInfoLabelList[i * 2]];
-				}
-
-				chrome.storage.sync.set({
-					// Saving General options
-					"popupcolor": popupcolor,
-					"highlight": highlight,
-					"textboxhl": textboxhl,
-					"onlyreading": onlyreading,
-					"minihelp": minihelp,
-					"disablekeys": disablekeys,
-					"kanjicomponents": kanjicomponents,
-					"kanjiInfo":kanjiInfoObject,
-					"popupLocation":popupLocation,
-					// Saving Copy to Clipboard settings
-					"lineEnding": lineEnding,
-					"copySeparator": copySeparator,
-					"maxClipCopyEntries": maxClipCopyEntries,
-					"popupDelay": popupDelay,
-					"showOnKey":showOnKey
-				});
-
-				chrome.extension.getBackgroundPage().rcxMain.config.css = popupcolor;
-				chrome.extension.getBackgroundPage().rcxMain.config.highlight = highlight;
-				chrome.extension.getBackgroundPage().rcxMain.config.textboxhl = textboxhl;
-				chrome.extension.getBackgroundPage().rcxMain.config.onlyreading = onlyreading;
-				chrome.extension.getBackgroundPage().rcxMain.config.minihelp = minihelp;
-				chrome.extension.getBackgroundPage().rcxMain.config.popupDelay = popupDelay;
-				chrome.extension.getBackgroundPage().rcxMain.config.disablekeys = disablekeys;
-				chrome.extension.getBackgroundPage().rcxMain.config.showOnKey = showOnKey;
-				chrome.extension.getBackgroundPage().rcxMain.config.kanjicomponents = kanjicomponents;
-				chrome.extension.getBackgroundPage().rcxMain.config.kanjiinfo = kanjiinfoarray;
-				chrome.extension.getBackgroundPage().rcxMain.config.lineEnding = lineEnding;
-				chrome.extension.getBackgroundPage().rcxMain.config.copySeparator = copySeparator;
-				chrome.extension.getBackgroundPage().rcxMain.config.maxClipCopyEntries = maxClipCopyEntries;
-				chrome.extension.getBackgroundPage().rcxMain.config.popupLocation = popupLocation;
-
-			}
-
-
-			function getPopUpDelay(){
-				// Todo: Why do we need a popup delay?
-				var popupDelay;
-				try {
-					popupDelay = parseInt(document.optform.popupDelay.value);
-					if (!isFinite(popupDelay)) {
-						throw Error('infinite');
+					// Setting Kanji values
+					for (i = 0; i*2 < kanjiInfoLabelList.length; i++) {
+						kanjiInfoObject[kanjiInfoLabelList[i * 2]] = document.getElementById(kanjiInfoLabelList[i * 2]).checked;
+						//Todo: Figure out why there are two duplicate arrays...
+						kanjiinfoarray[i] = kanjiInfoObject[kanjiInfoLabelList[i * 2]];
 					}
-				} catch (err) {
-					popupDelay = 150;
-					popupDelay: "150";
+
+					chrome.storage.sync.set({
+						// Saving General options
+						"popupcolor": popupcolor,
+						"highlight": highlight,
+						"textboxhl": textboxhl,
+						"onlyreading": onlyreading,
+						"minihelp": minihelp,
+						"disablekeys": disablekeys,
+						"kanjicomponents": kanjicomponents,
+						"kanjiInfo":kanjiInfoObject,
+						"popupLocation":popupLocation,
+						// Saving Copy to Clipboard settings
+						"lineEnding": lineEnding,
+						"copySeparator": copySeparator,
+						"maxClipCopyEntries": maxClipCopyEntries,
+						"popupDelay": popupDelay,
+						"showOnKey":showOnKey
+					});
+
+					chrome.extension.getBackgroundPage().rcxMain.config.css = popupcolor;
+					chrome.extension.getBackgroundPage().rcxMain.config.highlight = highlight;
+					chrome.extension.getBackgroundPage().rcxMain.config.textboxhl = textboxhl;
+					chrome.extension.getBackgroundPage().rcxMain.config.onlyreading = onlyreading;
+					chrome.extension.getBackgroundPage().rcxMain.config.minihelp = minihelp;
+					chrome.extension.getBackgroundPage().rcxMain.config.popupDelay = popupDelay;
+					chrome.extension.getBackgroundPage().rcxMain.config.disablekeys = disablekeys;
+					chrome.extension.getBackgroundPage().rcxMain.config.showOnKey = showOnKey;
+					chrome.extension.getBackgroundPage().rcxMain.config.kanjicomponents = kanjicomponents;
+					chrome.extension.getBackgroundPage().rcxMain.config.kanjiinfo = kanjiinfoarray;
+					chrome.extension.getBackgroundPage().rcxMain.config.lineEnding = lineEnding;
+					chrome.extension.getBackgroundPage().rcxMain.config.copySeparator = copySeparator;
+					chrome.extension.getBackgroundPage().rcxMain.config.maxClipCopyEntries = maxClipCopyEntries;
+					chrome.extension.getBackgroundPage().rcxMain.config.popupLocation = popupLocation;
+
 				}
-				return popupDelay;
+
+
+				function getPopUpDelay(){
+					var popupDelay;
+					try {
+						popupDelay = parseInt(document.optform.popupDelay.value);
+						if (!isFinite(popupDelay)) {
+							throw Error('infinite');
+						}
+					} catch (err) {
+						popupDelay = 150;
+						popupDelay: "150";
+					}
+					return popupDelay;
+				}
+				window.onload = get_options;
+
+				/*function clicktab(tab) {
+					selectedtab = document.getElementById(tab);
+					// change format of all tabs to deselected
+					// change format of selected tab to selected
+					// hide all tab contents
+					// show selected tab contents
+				}*/
+
+
+				document.querySelector('#submit').addEventListener('click', save_options);
+
+
+
+				document.optform.lineEnding[i].selected = true;
+				break;
 			}
-			window.onload = get_options;
-
-			/*function clicktab(tab) {
-				selectedtab = document.getElementById(tab);
-				// change format of all tabs to deselected
-				// change format of selected tab to selected
-				// hide all tab contents
-				// show selected tab contents
-			}*/
-
-
-			document.querySelector('#submit').addEventListener('click', save_options);
-
-
-
-			document.optform.lineEnding[i].selected = true;
-			break;
-		}
 	}
 
 	for(var i=0; i < document.optform.copySeparator.length; ++i) {
