041b55695c36d15751da0140e65d66ab0328af83
SaltfishAmi <14184974+SaltfishAmi@users.noreply.github.com>
2020-06-06 01:17:18 +0800

    I am baka...skipped ぶ and ぷ
    also I noticed の is absent in the original code...or is that a feature?

--- src/data.ts
+++ src/data.ts
@@ -302,7 +302,7 @@ export class Dictionary {
     // unlike おう which is pronounced as 長音. Still some people do write
     // オーサカ but it's rare enough that we simply don't deal with it yet.
     const barLessWord = input
-      .replace(/([おこごそぞとどほぼぽもよょろをうくぐすずつづぬふむゆゅる])ー/g, '$1う')
+      .replace(/([おこごそぞとどのほぼぽもよょろをうくぐすずつづぬふぶぷむゆゅる])ー/g, '$1う')
       .replace(/([あかがさざただなはばぱまやゃらわ])ー/g, '$1あ')
       .replace(/([いきぎしじちぢにひびぴみり])ー/g, '$1い')
       .replace(/([えけげせぜてでねへべぺめれ])ー/g, '$1え');
