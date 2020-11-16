c63cb99b07e0a50dc697554f2765268c31658074
Brian Birtles <birtles@gmail.com>
2020-11-16 14:07:17 +0900

    Update unused-strings.sh

--- utils/unused-strings.sh
+++ utils/unused-strings.sh
@@ -12,11 +12,15 @@ errors=0
 mapfile -t messages < <(jq -r 'keys[]' ../_locales/en/messages.json)
 for message in ${messages[@]}; do
   if [[ $(rg ${message} ../{html,src,css} ../*.src | wc -l) == 0 ]];then
-    # content_names_tag_*, pos_label_*, and head_info_label_* currently get a
-    # free pass since we programmatically generate those.
+    # Skip groups for which we programmatically generate the keys.
     if [[ $message != content_names_tag_* && \
-          $message != pos_label_* && \
-          $message != head_info_label_* ]]; then
+          $message != dial_label_* && \
+          $message != field_label_* && \
+          $message != gloss_type_label_* && \
+          $message != head_info_label_* && \
+          $message != lang_label_* && \
+          $message != misc_label_* && \
+          $message != pos_label_* ]]; then
       echo ${message} has no matches!
       ((errors=errors+1))
     fi
