136fae1ece6fcd1adacd8708e6e487d15f833c60
SaltfishAmi <ami@saltfish.moe>
2020-12-04 00:21:58 +0800

    Refuse to (mis)convert transliterations of the digits

--- src/numbers.ts
+++ src/numbers.ts
@@ -23,6 +23,18 @@ const kanjiToNumberMap = new Map<string, number>([
   ['兆', 1000000000000],
 ]);
 
+function validNumber(c1: number, c2: number): boolean {
+  if (c2 >= 10000 && c1 < c2) return true;
+  if (c1 >= 10000 && c2 <= 1000) return true;
+  if (c1 >= 100 && c2 < c1 && c2 >= 10 && c2 <= 1000) return true;
+  if (c1 == 1 && (c2 == 10 || c2 == 100)) return false;
+
+  const v1 = c1 < 10;
+  const v2 = c2 < 10;
+  if ((v1 || v2) && !(v1 && v2)) return true;
+  else return false;
+}
+
 export function kanjiToNumber(text: string): number | null {
   const stuffs = [...text].map((ch) => kanjiToNumberMap.get(ch));
   if (!stuffs.length || stuffs.some((ch) => typeof ch === 'undefined')) {
@@ -34,7 +46,7 @@ export function kanjiToNumber(text: string): number | null {
 
   while (numbers.length > 1) {
     const [first, second, ...rest] = numbers;
-
+    if (!validNumber(first, second)) return null;
     if (second < first) {
       if (rest.some((x) => x > first)) {
         numbers = breakDownNumbers(numbers);
