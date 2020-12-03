be335aacb9e2fe169b3f71881d5203514aec77e4
SaltfishAmi <ami@saltfish.moe>
2020-12-04 07:50:55 +0800

    Refuse to (mis)convert transliterations of kanji digits (#452)
    see: [#373 (comment)](https://github.com/birtles/rikaichamp/issues/373#issuecomment-689976608)
    
    Co-authored-by: Brian Birtles <birtles@gmail.com>

--- src/numbers.test.ts
+++ src/numbers.test.ts
@@ -4,7 +4,13 @@ describe('kanjiToNumber', () => {
   it('converts various numbers', () => {
     expect(kanjiToNumber('一')).toEqual(1);
     expect(kanjiToNumber('五十六')).toEqual(56);
+    expect(kanjiToNumber('五百十六')).toEqual(516);
     expect(kanjiToNumber('五十万一')).toEqual(500001);
+    expect(kanjiToNumber('五億五万一')).toEqual(500050001);
+    expect(kanjiToNumber('五万億五万一')).toEqual(5000000050001);
+    expect(kanjiToNumber('五十万億百万一')).toEqual(50000001000001);
+    expect(kanjiToNumber('七二一三五六四九')).toStrictEqual(null);
+    expect(kanjiToNumber('七十二百一')).toStrictEqual(null);
     expect(kanjiToNumber('abc')).toStrictEqual(null);
     expect(kanjiToNumber('')).toStrictEqual(null);
   });
