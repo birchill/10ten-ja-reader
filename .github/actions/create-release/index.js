5431790aec205f33a4058f3583b633eca16a290d
Brian Birtles <birtles@gmail.com>
2021-03-01 11:55:47 +0900

    chore: Start reworking release machinery

---
+++ .github/actions/create-release/index.js
@@ -0,0 +1,88 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+const path = require('path');
+
+async function main() {
+  const {
+    repo: { owner, repo },
+    sha,
+  } = context;
+  const prerelease = core.getInput('prerelease').toLowerCase() === 'true';
+  const version = core.getInput('version').toLowerCase();
+
+  const release = await github.repos.createRelease({
+    owner,
+    repo,
+    tag_name: process.env.GITHUB_REF,
+    name: `Release ${process.env.GITHUB_REF} (test)`,
+    draft: true,
+    prerelease,
+    target_commitish: sha,
+  });
+
+  const root = path.join(__dirname, '..', '..', '..');
+
+  // Upload Firefox asset
+  const firefoxPackageName = core.getInput('firefox_package_name');
+  const firefoxPackagePath = path.join(
+    root,
+    'dist-firefox-package',
+    firefoxPackageName
+  );
+  await github.repos.uploadReleaseAsset({
+    owner,
+    repo,
+    release_id: release.data.id,
+    name: `rikaichamp-${version}-firefox.zip`,
+    data: fs.readFileSync(firefoxPackagePath),
+  });
+
+  // Upload Chrome asset
+  const chromePackageName = core.getInput('chrome_package_name');
+  const chromePackagePath = path.join(
+    root,
+    'dist-firefox-package',
+    chromePackageName
+  );
+  await github.repos.uploadReleaseAsset({
+    owner,
+    repo,
+    release_id: release.data.id,
+    name: `rikaichamp-${version}-chrome.zip`,
+    data: fs.readFileSync(chromePackagePath),
+  });
+
+  // Upload source asset
+  const sourcePackagePath = path.join(
+    root,
+    'dist-src',
+    `rikaichamp-${version}-src.zip`
+  );
+  await github.repos.uploadReleaseAsset({
+    owner,
+    repo,
+    release_id: release.data.id,
+    name: `rikaichamp-${version}-src.zip`,
+    data: fs.readFileSync(sourcePackagePath),
+  });
+
+  // Upload raw source files
+  for (const file of fs.readdirSync(path.join(root, 'dist-firefox'))) {
+    if (!file.startsWith('rikaichamp-') || !file.endsWith('.js')) {
+      continue;
+    }
+
+    await github.repos.uploadReleaseAsset({
+      owner,
+      repo,
+      release_id: release.data.id,
+      name: file,
+      data: fs.readFileSync(file),
+    });
+  }
+}
+
+main().catch((error) => {
+  core.setFailed(error.message);
+});
