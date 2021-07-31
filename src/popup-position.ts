9d583c4e1d5df63a41f2e879dfe2579456173015
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2021-07-31 15:23:21 +0100

    feat: Popup now respects safe area and Puck
    fine-tune based on usage on iOS 15
    
    chore: Simplify safe area getters
    
    chore: Drop unused puck dimensions
    
    chore: Make the safe area insets a parameter passed into the popup positioning method
    
    chore: Simplify puck lifetime management (and fix theme toggling too)
    
    chore: Use the safe area insets to determine which vertical height to use
    
    chore: Don't constraint the width/height of the popup when it is manually positioned
    
    chore: Factor out clearTooltips parameter
    
    chore: Simplify popup manual positioning modes
    
    These modes are a niche feature (disabled by default) and should not
    try to avoid the puck or be constrained. They are engaged (using
    keyboard shortcuts) when the user specifically wants to override the
    auto positioning so they should be as simple and predictable as
    possible.
    
    (The approach before this patch wasn't working for me either -- it would
    often position the window so that it was overflowing the right edge and
    couldn't be read.)
    
    chore: Factor out common cursor clearance code
    
    chore: Drop alignment with top of moon
    
    I can't see that it makes a significant difference (it's only 5px
    anyway). After the popup shows, it's possible (and common) to move the
    moon up and down and the popup position won't change so any alignment
    will be lost.
    
    On the other hand, passing this parameter around complicates the logic
    and interfaces quite a bit so I think it's best to drop it unless we're
    sure it's necessary.
    
    chore: Factor out a parameter for specifying the preferred side of the popup
    
    chore: Drop no-longer-needed comment
    
    chore: Try factoring out common above/below positioning code
    
    chore: Try factoring out common left/right positioning code
    
    chore: Rework layout sorting to handling undefined elements

--- src/popup-position.ts
+++ src/popup-position.ts
@@ -1,5 +1,5 @@
 import { Point } from './geometry';
-import type { RikaiPuck } from './puck';
+import type { SafeAreaInsets } from './puck';
 
 export const enum PopupPositionMode {
   Start,
@@ -14,72 +14,86 @@ export const enum PopupPositionMode {
 const GUTTER = 5;
 
 export function getPopupPosition({
+  cursorClearance,
   doc,
   isVerticalText,
-  puck,
   mousePos,
   popupSize,
   positionMode,
-  targetHasTitle,
+  preferredSide,
+  safeArea: initialSafeArea,
 }: {
+  cursorClearance: SafeAreaInsets;
   doc: Document;
   isVerticalText: boolean;
-  puck: RikaiPuck | null;
   mousePos?: Point;
   popupSize: { width: number; height: number };
   positionMode: PopupPositionMode;
-  targetHasTitle: boolean;
-}): {
-  x: number;
-  y: number;
-  constrainWidth: number | null;
-  constrainHeight: number | null;
-} {
+  preferredSide: 'above' | 'below';
+  safeArea: SafeAreaInsets;
+}): PopupPosition {
   const { scrollX, scrollY } = doc.defaultView!;
+
   // Use the clientWidth (as opposed to doc.defaultView.innerWidth) since this
   // excludes the width of any scrollbars.
   const stageWidth = doc.documentElement.clientWidth;
 
-  // For the height, however, we need to be careful because:
-  // - in quirks mode, the body has the viewport height;
-  // - when the puck is in use, we must consider the safe area insets, and thus
-  //   doc.defaultView.innerHeight must be used. See comments in puck.ts, in the
-  //   getViewportDimensions() function, for more details.
-  const stageHeight = puck
-    ? doc.defaultView!.innerHeight
-    : doc.compatMode === 'BackCompat'
-    ? doc.body?.clientHeight || doc.defaultView!.innerHeight
-    : doc.documentElement.clientHeight;
+  // For the height, we'd like to similarly use clientHeight...
+  let stageHeight = document.documentElement.clientHeight;
+
+  // ... but we need to be careful because:
+  //
+  // (a) in quirks mode, the body has the viewport height;
+  if (doc.compatMode === 'BackCompat') {
+    stageHeight = doc.body?.clientHeight || doc.defaultView!.innerHeight;
+  }
+
+  // (b) at least in iOS 15 Safari, the safe area appears to be measured from
+  // the innerHeight so if we have a non-zero vertical safe area inset, we
+  // should use the innerHeight instead.
+  if (initialSafeArea.top !== 0 || initialSafeArea.bottom !== 0) {
+    stageHeight = document.defaultView!.innerHeight;
+  }
+
+  // Now that we have finished detecting the absence/presence of a vertical safe
+  // area, merge our gutter into the safe area.
+  const safeArea = {
+    left: initialSafeArea.left + GUTTER,
+    right: initialSafeArea.right + GUTTER,
+    top: initialSafeArea.top + GUTTER,
+    bottom: initialSafeArea.bottom + GUTTER,
+  };
 
   if (positionMode === PopupPositionMode.Auto) {
     return getAutoPosition({
+      cursorClearance,
       isVerticalText,
-      puck,
       mousePos,
       popupSize,
+      preferredSide,
+      safeArea,
       scrollX,
       scrollY,
-      targetHasTitle,
       stageWidth,
       stageHeight,
     });
   }
 
-  const availableStageHeight = stageHeight - 2 * GUTTER;
+  const availableStageHeight = stageHeight - (safeArea.top + safeArea.bottom);
 
-  const left = scrollX + GUTTER;
-  const top = scrollY + GUTTER;
-  const right = scrollX + stageWidth - popupSize.width - GUTTER;
+  const left = scrollX + safeArea.left;
+  const top = scrollY + safeArea.top;
+  const right = scrollX + stageWidth - popupSize.width - safeArea.right;
   const bottom =
     scrollY +
     stageHeight -
     Math.min(popupSize.height, availableStageHeight) -
-    GUTTER;
+    safeArea.bottom;
 
   // We could calculate a value for constrainHeight as something like:
   //
-  //   constrainHeight = popupSize.height > availableStageHeight
-  //                     ? availableStageHeight
+  //   constrainHeight = popupSize.height > availableWindowHeight
+  //                     ? availableWindowHeight
   //                     : null;
   //
   // and we'd get the nice fade effect to show in that case, but it's probably
@@ -105,87 +119,267 @@ export function getPopupPosition({
   }
 }
 
-function getAutoPosition({
-  isVerticalText,
-  puck,
-  mousePos,
-  popupSize,
-  scrollX,
-  scrollY,
-  targetHasTitle,
-  stageWidth,
-  stageHeight,
-}: {
-  isVerticalText: boolean;
-  puck: RikaiPuck | null;
-  mousePos?: Point;
-  popupSize: { width: number; height: number };
-  scrollX: number;
-  scrollY: number;
-  targetHasTitle: boolean;
-  stageWidth: number;
-  stageHeight: number;
-}): {
+interface PopupPosition {
   x: number;
   y: number;
   constrainWidth: number | null;
   constrainHeight: number | null;
-} {
-  let x = mousePos?.x || 0;
-  let y = mousePos?.y || 0;
+}
 
-  // Inline position: Go back (left or up) if necessary
-  //
-  // (We shouldn't need to check the opposite direction since the initial
-  // position, if set to something non-zero, is coming from a mouse event which
-  // should, I believe, be positive.)
-  let inline = isVerticalText ? y : x;
-  const inlinePopupSize = isVerticalText ? popupSize.height : popupSize.width;
-  const inlineStageSize = isVerticalText ? stageHeight : stageWidth;
-  if (inline + inlinePopupSize > inlineStageSize - GUTTER) {
-    inline = inlineStageSize - inlinePopupSize - GUTTER;
-    if (inline < 0) {
-      inline = 0;
+function getAutoPosition({
+  cursorClearance,
+  isVerticalText,
+  mousePos,
+  popupSize,
+  preferredSide,
+  safeArea,
+  scrollX,
+  scrollY,
+  stageWidth,
+  stageHeight,
+}: {
+  cursorClearance: SafeAreaInsets;
+  isVerticalText: boolean;
+  mousePos?: Point;
+  popupSize: { width: number; height: number };
+  preferredSide: 'above' | 'below';
+  safeArea: SafeAreaInsets;
+  scrollX: number;
+  scrollY: number;
+  stageWidth: number;
+  stageHeight: number;
+}): PopupPosition {
+  const x = mousePos?.x || 0;
+  const y = mousePos?.y || 0;
+
+  const { left: safeLeft, top: safeTop } = safeArea;
+  const safeRight = stageWidth - safeArea.right;
+  const safeBottom = stageHeight - safeArea.bottom;
+
+  const marginToPopup = 25;
+
+  const popupAboveLayout = getAboveOrBelowPosition({
+    cursorClearance,
+    marginToPopup,
+    popupSize,
+    position: 'above',
+    safeBoundaries: { safeLeft, safeRight, safeTop, safeBottom },
+    target: { x, y },
+  });
+  const popupRightLayout = getLeftOrRightPosition({
+    cursorClearance,
+    marginToPopup,
+    popupSize,
+    position: 'right',
+    safeBoundaries: { safeLeft, safeRight, safeTop, safeBottom },
+    target: { x, y },
+  });
+  const popupLeftLayout = getLeftOrRightPosition({
+    cursorClearance,
+    marginToPopup,
+    popupSize,
+    position: 'left',
+    safeBoundaries: { safeLeft, safeRight, safeTop, safeBottom },
+    target: { x, y },
+  });
+  const popupBelowLayout = getAboveOrBelowPosition({
+    cursorClearance,
+    marginToPopup,
+    popupSize,
+    position: 'below',
+    safeBoundaries: { safeLeft, safeRight, safeTop, safeBottom },
+    target: { x, y },
+  });
+
+  // Build up preferred order for considering each layout
+  const leftAndRightPreference = [popupRightLayout, popupLeftLayout];
+  const belowAndAbovePreference =
+    preferredSide === 'above'
+      ? [popupAboveLayout, popupBelowLayout]
+      : [popupBelowLayout, popupAboveLayout];
+  const orderOfPreference = isVerticalText
+    ? [...leftAndRightPreference, ...belowAndAbovePreference]
+    : [...belowAndAbovePreference, ...leftAndRightPreference];
+
+  // We generally prefer to use a layout in the block direction
+  const orthogonalLayouts = orderOfPreference.slice(0, 2);
+  const preferredLayoutWithoutBlockConstraint = orthogonalLayouts.find(
+    (l) => l && (isVerticalText ? l.constrainWidth : l.constrainHeight) === null
+  );
+
+  // If we can't find an unconstrained layout in the block direction fall back
+  // to the layout with the highest area.
+  const preferredLayout =
+    preferredLayoutWithoutBlockConstraint ??
+    orderOfPreference.sort(sizeComparator(popupSize))[0];
+
+  // This probably never happens but if we have no suitable layouts, just put
+  // the popup in the top-left.
+  if (!preferredLayout) {
+    return {
+      x: safeLeft,
+      y: safeTop,
+      constrainWidth: null,
+      constrainHeight: null,
+    };
+  }
+
+  return {
+    ...preferredLayout,
+    x: preferredLayout.x + scrollX,
+    y: preferredLayout.y + scrollY,
+  };
+}
+
+// TODO: Combine the below two functions into a more generic one.
+
+function getAboveOrBelowPosition({
+  cursorClearance,
+  marginToPopup,
+  position,
+  popupSize,
+  safeBoundaries: { safeLeft, safeRight, safeTop, safeBottom },
+  target,
+}: {
+  cursorClearance: SafeAreaInsets;
+  marginToPopup: number;
+  popupSize: { width: number; height: number };
+  position: 'above' | 'below';
+  safeBoundaries: {
+    safeLeft: number;
+    safeRight: number;
+    safeTop: number;
+    safeBottom: number;
+  };
+  target: { x: number; y: number };
+}): PopupPosition | undefined {
+  // Horizontal position
+  const idealX = target.x;
+  const x =
+    idealX + popupSize.width > safeRight
+      ? Math.max(safeLeft, safeRight - popupSize.width)
+      : idealX;
+  const constrainWidth = x + popupSize.width > safeRight ? safeRight - x : null;
+
+  // Vertical position
+  let y;
+  let constrainHeight: number | null;
+  if (position === 'above') {
+    const marginBetweenMouseEventAndBottomOfPopup =
+      marginToPopup + cursorClearance.top;
+    const maxYExtent = target.y - marginBetweenMouseEventAndBottomOfPopup;
+
+    y = Math.max(safeTop, maxYExtent - popupSize.height);
+    if (y >= maxYExtent) {
+      return undefined;
     }
-  }
 
-  // Block position: Position "below" the mouse cursor
-  let blockAdjust = 25;
+    constrainHeight = y + popupSize.height > maxYExtent ? maxYExtent - y : null;
+  } else {
+    const marginBetweenMouseEventAndTopOfPopup =
+      cursorClearance.bottom + marginToPopup;
+    y = target.y + marginBetweenMouseEventAndTopOfPopup;
 
-  // If the element has a title, then there will probably be
-  // a tooltip that we shouldn't cover up.
-  if (targetHasTitle) {
-    blockAdjust += 20;
-  }
-
-  // Check if we are too close to the stage edge (bottom / right)...
-  let block = isVerticalText ? x : y;
-  const blockPopupSize = isVerticalText ? popupSize.width : popupSize.height;
-  const blockStageSize = isVerticalText ? stageWidth : stageHeight;
-  let constrainBlockSize: number | null = null;
-  if (block + blockAdjust + blockPopupSize > blockStageSize) {
-    // ...we are. See if the other side has more room.
-    const spaceOnThisSide = blockStageSize - block;
-    const spaceOnOtherSide = block;
-    if (spaceOnOtherSide > spaceOnThisSide) {
-      blockAdjust = Math.max(-blockPopupSize - 25, -block);
-      if (spaceOnOtherSide - 25 < blockPopupSize) {
-        constrainBlockSize = spaceOnOtherSide - 25;
-      }
+    if (y >= safeBottom) {
+      return undefined;
     }
+
+    constrainHeight = y + popupSize.height > safeBottom ? safeBottom - y : null;
   }
-
-  block += blockAdjust;
-
-  // De-logicalize before we add the scroll position since that's phyiscal
-  x = isVerticalText ? block : inline;
-  y = isVerticalText ? inline : block;
-  const constrainHeight = !isVerticalText ? constrainBlockSize : null;
-  const constrainWidth = isVerticalText ? constrainBlockSize : null;
-
-  // Adjust for scroll position
-  x += scrollX;
-  y += scrollY;
 
   return { x, y, constrainWidth, constrainHeight };
+}
+
+function getLeftOrRightPosition({
+  cursorClearance,
+  marginToPopup,
+  position,
+  popupSize,
+  safeBoundaries: { safeLeft, safeRight, safeTop, safeBottom },
+  target,
+}: {
+  cursorClearance: SafeAreaInsets;
+  marginToPopup: number;
+  popupSize: { width: number; height: number };
+  position: 'left' | 'right';
+  safeBoundaries: {
+    safeLeft: number;
+    safeRight: number;
+    safeTop: number;
+    safeBottom: number;
+  };
+  target: { x: number; y: number };
+}): PopupPosition | undefined {
+  // Horizontal position
+  let x;
+  let constrainWidth: number | null;
+  if (position === 'left') {
+    const marginBetweenMouseEventAndRightOfPopup =
+      cursorClearance.left + marginToPopup;
+    const maxXExtent = target.x - marginBetweenMouseEventAndRightOfPopup;
+
+    x = Math.max(safeLeft, maxXExtent - popupSize.width);
+    if (x >= maxXExtent) {
+      return undefined;
+    }
+
+    constrainWidth = x + popupSize.width > maxXExtent ? maxXExtent - x : null;
+  } else {
+    const marginBetweenMouseEventAndLeftOfPopup =
+      cursorClearance.right + marginToPopup;
+
+    x = target.x + marginBetweenMouseEventAndLeftOfPopup;
+    if (x >= safeRight) {
+      return undefined;
+    }
+
+    constrainWidth = x + popupSize.width > safeRight ? safeRight - x : null;
+  }
+
+  // Vertical position
+  const idealY = target.y;
+  const y =
+    idealY + popupSize.height > safeBottom
+      ? Math.max(safeTop, safeBottom - popupSize.height)
+      : idealY;
+  if (safeBottom - safeTop < 0) {
+    return undefined;
+  }
+  const constrainHeight =
+    y + popupSize.height > safeBottom ? safeBottom - safeTop : null;
+
+  return { x, y, constrainWidth, constrainHeight };
+}
+
+function sizeComparator(popupSize: { width: number; height: number }) {
+  return (
+    a: PopupPosition | undefined,
+    b: PopupPosition | undefined
+  ): number => {
+    // Sort undefined entries last
+    if (!b) {
+      return 0;
+    }
+
+    if (!a) {
+      return 1;
+    }
+
+    const widthA = a.constrainWidth ?? popupSize.width;
+    const heightA = a.constrainHeight ?? popupSize.height;
+    const areaA = widthA * heightA;
+
+    const widthB = b.constrainWidth ?? popupSize.width;
+    const heightB = b.constrainHeight ?? popupSize.height;
+    const areaB = widthB * heightB;
+
+    if (widthA === widthB) {
+      return areaB - areaA;
+    }
+
+    // Prefer wider results wherever possible, as it's okay to lose a few entries
+    // from the bottom of the popup, but disastrous for all the entries to be
+    // clipped off on the right.
+    return widthB - widthA;
+  };
 }
