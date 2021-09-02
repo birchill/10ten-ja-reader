birchill/10ten-ja-reader

commit ab73fd4eeb1fa96269405e49abba837861319436

feat: Make popup avoid the selected text
Revert "fix: invalidate the safe area cache before popup positioning"

This reverts commit 71980e267827f67e7d84437ad561f43a2d1df9e0.

src/content.ts
@@ -59,7 +59,13 @@ import {
   Entry as CopyEntry,
 } from './copy-text';
 import { isEditableNode, isMessageSourceWindow } from './dom-utils';
-import { Point } from './geometry';
+import {
+  addMarginToPoint,
+  getMarginAroundPoint,
+  MarginBox,
+  Point,
+  union,
+} from './geometry';
 import { getTextAtPoint } from './get-text';
 import { getIframeOriginFromWindow } from './iframe-tracker';
 import { SelectionMeta } from './meta';
@@ -75,11 +81,15 @@ import {
   setPopupStyle,
 } from './popup';
 import { getPopupPosition, PopupPositionMode } from './popup-position';
-import { removePuck, RikaiPuck, SafeAreaInsets } from './puck';
+import { removePuck, RikaiPuck } from './puck';
 import { query, QueryResult } from './query';
 import { isForeignObjectElement, isSvgDoc, isSvgSvgElement } from './svg';
 import { stripFields } from './strip-fields';
-import { getTargetElementProps, TargetProps } from './target-props';
+import {
+  getBestFitSize,
+  getTargetElementProps,
+  TargetProps,
+} from './target-props';
 import { TextHighlighter } from './text-highlighter';
 import { TextRange, textRangesEqual } from './text-range';
 import { hasReasonableTimerResolution } from './timer-precision';
@@ -1039,7 +1049,10 @@ export class ContentHandler {
       meta: textAtPoint.meta,
       source: null,
       text: textAtPoint.text,
-      targetProps: getTargetElementProps(eventElement),
+      targetProps: getTargetElementProps({
+        target: eventElement,
+        textRange: textAtPoint?.textRange || undefined,
+      }),
       wordLookup: !!textAtPoint.textRange,
     };
 
@@ -1193,17 +1206,7 @@ export class ContentHandler {
   }
 
   highlightTextForCurrentResult() {
-    if (this.config.noTextHighlight) {
-      return;
-    }
-
-    const searchResult = this.currentSearchResult?.[this.currentDict];
-
-    // Work out the appropriate length to highlight
-    const highlightLength = Math.max(
-      searchResult?.matchLen || 0,
-      this.currentLookupParams?.meta?.matchLen || 0
-    );
+    const highlightLength = this.getHighlightLengthForCurrentResult();
 
     // Check we have something to highlight
     if (highlightLength < 1) {
@@ -1219,6 +1222,23 @@ export class ContentHandler {
     }
 
     this.highlightText(highlightLength);
+  }
+
+  getHighlightLengthForCurrentResult(): number {
+    if (this.config.noTextHighlight) {
+      return 0;
+    }
+
+    if (!this.currentSearchResult) {
+      return 0;
+    }
+
+    const searchResult = this.currentSearchResult[this.currentDict];
+
+    return Math.max(
+      searchResult?.matchLen || 0,
+      this.currentLookupParams?.meta?.matchLen || 0
+    );
   }
 
   showPopup(options?: { copyState?: CopyState; copyType?: CopyType }) {
@@ -1288,10 +1308,10 @@ export class ContentHandler {
       bottom: 0,
     };
 
-    // TODO: Set this based on whether the most recent mousemove event was
-    // generated by the puck or not, not simply on whether or not the puck is
+    // TODO: Set this based on whether or not the most recent mousemove event
+    // was generated by the puck, not simply on whether or not the puck is
     // available.
-    let cursorClearance: SafeAreaInsets;
+    let cursorClearance: MarginBox;
     if (this.puck) {
       const {
         puckAboveMouseEvent: top,
@@ -1306,18 +1326,42 @@ export class ContentHandler {
       const extraMarginToClearThumb = 100;
       cursorClearance = {
         top,
+        right,
         bottom: bottom + extraMarginToClearThumb,
         left,
-        right,
       };
     } else {
       const tooltipClearance = !!this.currentTargetProps?.hasTitle ? 20 : 0;
       cursorClearance = {
         top: 0,
         right: 0,
-        left: 0,
         bottom: tooltipClearance,
+        left: 0,
       };
+    }
+
+    // Add the first part of the matched text to the cursor clearance.
+    //
+    // We don't want to add _all_ of it since we might have a selection that
+    // wraps lines and that would produce a massive area that would be too hard
+    // to avoid.
+    const { textBoxSizes } = this.currentTargetProps || {};
+    if (textBoxSizes && this.currentPoint) {
+      const bbox = getBestFitSize({
+        sizes: textBoxSizes,
+        length: this.getHighlightLengthForCurrentResult(),
+      });
+      if (bbox) {
+        const cursorClearanceAsRect = addMarginToPoint(
+          cursorClearance,
+          this.currentPoint
+        );
+        const expandedClearance = union(bbox, cursorClearanceAsRect);
+        cursorClearance = getMarginAroundPoint(
+          this.currentPoint,
+          expandedClearance
+        );
+      }
     }
 
     const {

src/geometry.ts
@@ -1,7 +1,60 @@
+export type Rect = {
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+};
+
 export type Point = {
   x: number;
   y: number;
 };
+
+export type Box = {
+  top: number;
+  right: number;
+  bottom: number;
+  left: number;
+};
+
+/// Measures from some box (or point) outwards
+export type MarginBox = Box;
+
+/// Measures from some box inwards
+export type PaddingBox = Box;
+
+// Conversion utilities
+
+export function addMarginToPoint(margin: MarginBox, point: Point): Rect {
+  return {
+    left: point.x - margin.left,
+    top: point.y - margin.top,
+    width: margin.left + margin.right,
+    height: margin.top + margin.bottom,
+  };
+}
+
+export function getMarginAroundPoint(point: Point, rect: Rect): MarginBox {
+  return {
+    left: point.x - rect.left,
+    top: point.y - rect.top,
+    right: rect.left + rect.width - point.x,
+    bottom: rect.top + rect.height - point.y,
+  };
+}
+
+// Geometry utils
+
+export function union(a: Rect, b: Rect): Rect {
+  return {
+    left: Math.min(a.left, b.left),
+    top: Math.min(a.top, b.top),
+    width:
+      Math.max(a.left + a.width, b.left + b.width) - Math.min(a.left, b.left),
+    height:
+      Math.max(a.top + a.height, b.top + b.height) - Math.min(a.top, b.top),
+  };
+}
 
 export function bboxIncludesPoint({
   bbox,

src/popup-position.ts
@@ -1,5 +1,4 @@
-import { Point } from './geometry';
-import type { SafeAreaInsets } from './puck';
+import { MarginBox, PaddingBox, Point } from './geometry';
 
 export const enum PopupPositionMode {
   Start,
@@ -23,14 +22,14 @@ export function getPopupPosition({
   preferredSide,
   safeArea: initialSafeArea,
 }: {
-  cursorClearance: SafeAreaInsets;
+  cursorClearance: MarginBox;
   doc: Document;
   isVerticalText: boolean;
   mousePos?: Point;
   popupSize: { width: number; height: number };
   positionMode: PopupPositionMode;
   preferredSide: 'above' | 'below';
-  safeArea: SafeAreaInsets;
+  safeArea: PaddingBox;
 }): PopupPosition {
   const { scrollX, scrollY } = doc.defaultView!;
 
@@ -138,12 +137,12 @@ function getAutoPosition({
   stageWidth,
   stageHeight,
 }: {
-  cursorClearance: SafeAreaInsets;
+  cursorClearance: MarginBox;
   isVerticalText: boolean;
   mousePos?: Point;
   popupSize: { width: number; height: number };
   preferredSide: 'above' | 'below';
-  safeArea: SafeAreaInsets;
+  safeArea: PaddingBox;
   scrollX: number;
   scrollY: number;
   stageWidth: number;
@@ -241,7 +240,7 @@ function getAboveOrBelowPosition({
   safeBoundaries: { safeLeft, safeRight, safeTop, safeBottom },
   target,
 }: {
-  cursorClearance: SafeAreaInsets;
+  cursorClearance: MarginBox;
   marginToPopup: number;
   popupSize: { width: number; height: number };
   position: 'above' | 'below';
@@ -251,7 +250,7 @@ function getAboveOrBelowPosition({
     safeTop: number;
     safeBottom: number;
   };
-  target: { x: number; y: number };
+  target: Point;
 }): PopupPosition | undefined {
   // Horizontal position
   const idealX = target.x;
@@ -298,7 +297,7 @@ function getLeftOrRightPosition({
   safeBoundaries: { safeLeft, safeRight, safeTop, safeBottom },
   target,
 }: {
-  cursorClearance: SafeAreaInsets;
+  cursorClearance: MarginBox;
   marginToPopup: number;
   popupSize: { width: number; height: number };
   position: 'left' | 'right';
@@ -308,7 +307,7 @@ function getLeftOrRightPosition({
     safeTop: number;
     safeBottom: number;
   };
-  target: { x: number; y: number };
+  target: Point;
 }): PopupPosition | undefined {
   // Horizontal position
   let x;

src/puck.ts
@@ -2,20 +2,14 @@ import {
   getOrCreateEmptyContainer,
   removeContentContainer,
 } from './content-container';
+import { PaddingBox } from './geometry';
+import { getThemeClass } from './themes';
 
 import puckStyles from '../css/puck.css';
-import { getThemeClass } from './themes';
 
 interface ViewportDimensions {
   viewportWidth: number;
   viewportHeight: number;
-}
-
-export interface SafeAreaInsets {
-  top: number;
-  right: number;
-  bottom: number;
-  left: number;
 }
 
 export class RikaiPuck {
@@ -46,7 +40,7 @@ export class RikaiPuck {
   private targetOffset: { x: number; y: number } = { x: 0, y: 0 };
   private targetOrientation: 'above' | 'below' = 'above';
   private cachedViewportDimensions: ViewportDimensions | null = null;
-  private cachedSafeAreaInsets: SafeAreaInsets | null = null;
+  private cachedSafeAreaInsets: PaddingBox | null = null;
   private isBeingDragged: boolean = false;
 
   private setPosition(x: number, y: number) {
@@ -146,7 +140,7 @@ export class RikaiPuck {
     return this.cachedViewportDimensions;
   }
 
-  public getSafeArea(): SafeAreaInsets | undefined {
+  public getSafeArea(): PaddingBox | undefined {
     if (!this.puck) {
       return undefined;
     }

src/target-props.ts
@@ -1,16 +1,107 @@
+import { isTextInputNode } from './dom-utils';
+import { Rect } from './geometry';
+import { TextRange } from './text-range';
+
 /// Properties about the target element from which we started lookup needed
 /// so that we can correctly position the popup in a way that doesn't overlap
 /// the element.
 export type TargetProps = {
   hasTitle: boolean;
+  textBoxSizes?: SelectionSizes;
   isVerticalText: boolean;
 };
 
-export function getTargetElementProps(target: Element): TargetProps {
+// A range of t-shirt sizes for the first part of the text selection.
+//
+// This is a bit awkward because in the iframe case it is the text-handling
+// window that has access to the element needed to get bounding box sizes.
+// However, it doesn't know how much of the text will be matched.
+//
+// We don't want to be doing IPC calls as part of the popup positioning so we
+// just return a set of standard sizes and let the topmost window choose the
+// best fit.
+export type SelectionSizes = {
+  4: Rect;
+  8: Rect;
+  12: Rect;
+  16: Rect;
+};
+
+// Guaranteed to be arranged in ascending order
+export const textBoxSizeLengths: ReadonlyArray<keyof SelectionSizes> = [
+  4, 8, 12, 16,
+];
+
+export function getTargetElementProps({
+  target,
+  textRange,
+}: {
+  target: Element;
+  textRange: TextRange | undefined;
+}): TargetProps {
   return {
     hasTitle: !!((target as HTMLElement) || null)?.title,
+    textBoxSizes: textRange
+      ? getInitialBboxOfTextSelection(textRange)
+      : undefined,
     isVerticalText: !!target.ownerDocument.defaultView
       ?.getComputedStyle(target)
       .writingMode.startsWith('vertical'),
   };
+}
+
+function getInitialBboxOfTextSelection(
+  textRange: TextRange
+): SelectionSizes | undefined {
+  // Check we actually have some text selection available
+  //
+  // (We easily can't get the bbox of text selections in input elements
+  // unfortunately.)
+  if (!textRange.length || isTextInputNode(textRange[0].node)) {
+    return undefined;
+  }
+
+  const node = textRange[0].node;
+  const range = node.ownerDocument!.createRange();
+  range.setStart(node, textRange[0].start);
+
+  let lastEnd = -1;
+  let lastSize: Rect | undefined;
+
+  const result: Partial<SelectionSizes> = {};
+  for (const size of textBoxSizeLengths) {
+    const end = Math.min(textRange[0].start + size, textRange[0].end);
+    if (end <= lastEnd) {
+      result[size] = lastSize!;
+    } else {
+      range.setEnd(node, end);
+      result[size] = range.getClientRects()[0];
+
+      lastEnd = end;
+      lastSize = result[size];
+    }
+  }
+
+  return result as SelectionSizes;
+}
+
+export function getBestFitSize({
+  sizes,
+  length,
+}: {
+  sizes: SelectionSizes;
+  length: number;
+}): Rect | undefined {
+  // If the length is zero, it's probably best to say no size
+  if (!length) {
+    return undefined;
+  }
+
+  // Otherwise, find the first size that is _bigger_ than the provided length.
+  // And if there is none, just choose the biggest size.
+  const bestFitSize =
+    textBoxSizeLengths.slice().find((len) => len > length) ||
+    textBoxSizeLengths[textBoxSizeLengths.length - 1];
+
+  return sizes[bestFitSize];
 }

commit 3fb94298e279c2bd5ddf75b7c1aba5a5bb335268

fix(regression): Restore highlighting of metadata-only matches
This regression was introduced by
ab73fd4eeb1fa96269405e49abba837861319436 (which was likely from a
botched rebase).

src/content.ts
@@ -1411,11 +1411,7 @@ export class ContentHandler {
       return 0;
     }
 
-    if (!this.currentSearchResult) {
-      return 0;
-    }
-
-    const searchResult = this.currentSearchResult[this.currentDict];
+    const searchResult = this.currentSearchResult?.[this.currentDict];
 
     return Math.max(
       searchResult?.matchLen || 0,