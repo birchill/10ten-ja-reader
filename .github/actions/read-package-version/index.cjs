e6beafad3cc0830ab4bcfa7afe5614a7b5a22af1
Brian Birtles <birtles@gmail.com>
2022-03-29 17:41:24 +0900

    chore: update actions to node 16

--- .github/actions/read-package-version/index.cjs
+++
@@ -1,9 +0,0 @@
-// Code from https://github.com/actions/runner/issues/772#issuecomment-920919080
-
-child = require('child_process');
-path = require('path');
-
-// fork to system-installed node which is v14.17.6 at this time of writing
-// this workaround can be removed once the Github actions runner supports a
-// newer version of Node.js
-child.fork(path.resolve(__dirname, 'index.mjs'), [], { execPath: 'node' });
