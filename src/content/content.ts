aa50a27dc6fbb9372978952955bb92f5906a371c
Brian Birtles <birtles@gmail.com>
2022-09-03 14:18:55 +0900

    fix: revert simplification to unpinning code
    If the cursor is still over the text we looked up, we want the popup to
    continue showing when you unpin it but the previous code would prevent
    that from happenning.

--- src/content/content.ts
+++ src/content/content.ts
@@ -2252,13 +2252,29 @@ export class ContentHandler {
     });
 
     // Typically when you unpin the popup you want it to disappear immediately
-    // (unless the mouse is currently over it).
+    // (unless the mouse is currently over it or still over the original text).
+    //
+    // To try to make that happen we dispatch another mouse event with the last
+    // mouse position.
     //
     // Unfortunately this won't necessarily help if the user has since moused
-    // over an iframe since our last recorded target element will be based on
-    // the last mousemove event we received in _this_ frame.
-    if (this.lastMouseTarget && !isPopupWindowHostElem(this.lastMouseTarget)) {
-      this.clearResult();
+    // over an iframe since our last recorded mouse position and target element
+    // will be based on the last mousemove event we received in _this_ frame.
+    if (this.currentPoint && this.lastMouseTarget) {
+      const mouseMoveEvent = new MouseEvent('mousemove', {
+        bubbles: true,
+        screenX: this.currentPoint.x,
+        screenY: this.currentPoint.y,
+        clientX: this.currentPoint.x,
+        clientY: this.currentPoint.y,
+        ctrlKey: false,
+        shiftKey: false,
+        altKey: false,
+        metaKey: false,
+        button: 0,
+        buttons: 0,
+      });
+      this.lastMouseTarget.dispatchEvent(mouseMoveEvent);
     }
   }
 
