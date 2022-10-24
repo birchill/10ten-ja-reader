922a283be438b55a1e8f2897453daffda214a1a5
Brian Birtles <birtles@gmail.com>
2022-10-24 16:46:48 +0900

    fix: queue up changes to the browser action
    Fixes #1003

--- src/background/browser-action.ts
+++ src/background/browser-action.ts
@@ -23,7 +23,39 @@ const throttledSetTitle = throttle(
   2500
 );
 
-export function updateBrowserAction({
+let currentUpdate: { next?: BrowserActionState } | undefined;
+
+export function updateBrowserAction(params: BrowserActionState) {
+  // If we are already trying to update the browser action, queue our changes,
+  // clobbering any other queued changes.
+  //
+  // We need to ensure only one update runs at a time because in Chromium-based
+  // browsers setIcon is not FIFO and we can have earlier changes clobbering
+  // later ones.
+  if (currentUpdate) {
+    currentUpdate.next = params;
+    return;
+  }
+
+  void (async () => {
+    let nextParams: BrowserActionState | undefined = params;
+    while (nextParams) {
+      try {
+        // Indicate an update is running
+        currentUpdate = {};
+        await doUpdateBrowserAction(nextParams);
+      } catch {
+        // Ignore
+      }
+      // Look for the next update
+      nextParams = currentUpdate?.next;
+    }
+    // No updates left
+    currentUpdate = undefined;
+  })();
+}
+
+async function doUpdateBrowserAction({
   enabled,
   jpdictState,
   tabId,
@@ -108,7 +140,7 @@ export function updateBrowserAction({
   // We'd like to feature-detect if SVG icons are supported but Safari will
   // just fail silently if we try.
   const iconFilename = iconFilenameParts.join('-');
-  void setIcon(iconFilename, tabId);
+  await setIcon(iconFilename, tabId);
 
   // Add a warning overlay and update the string if there was a fatal
   // update error.
@@ -124,22 +156,22 @@ export function updateBrowserAction({
     // a constant error signal.
     jpdictState.updateError.name !== 'QuotaExceededError'
   ) {
-    void browser.browserAction.setBadgeText({ text: '!', tabId });
-    void browser.composeAction?.setBadgeText({ text: '!' });
-    void browser.browserAction.setBadgeBackgroundColor({
+    await browser.browserAction.setBadgeText({ text: '!', tabId });
+    await browser.composeAction?.setBadgeText({ text: '!' });
+    await browser.browserAction.setBadgeBackgroundColor({
       color: 'yellow',
       tabId,
     });
-    void browser.composeAction?.setBadgeBackgroundColor({ color: 'yellow' });
+    await browser.composeAction?.setBadgeBackgroundColor({ color: 'yellow' });
     tooltip = browser.i18n.getMessage('command_toggle_update_error');
   } else {
-    void browser.browserAction.setBadgeText({ text: '', tabId });
-    void browser.composeAction?.setBadgeText({ text: '' });
+    await browser.browserAction.setBadgeText({ text: '', tabId });
+    await browser.composeAction?.setBadgeText({ text: '' });
   }
 
   // Set the caption
   throttledSetTitle({ title: tooltip, tabId });
-  void browser.composeAction?.setTitle({ title: tooltip });
+  await browser.composeAction?.setTitle({ title: tooltip });
 }
 
 async function setIcon(iconFilename: string, tabId?: number): Promise<void> {
@@ -147,8 +179,8 @@ async function setIcon(iconFilename: string, tabId?: number): Promise<void> {
   // just fail silently if we try.
   if (__SUPPORTS_SVG_ICONS__) {
     const details = { path: `images/${iconFilename}.svg` };
-    void browser.browserAction.setIcon({ ...details, tabId });
-    void browser.composeAction?.setIcon(details);
+    await browser.browserAction.setIcon({ ...details, tabId });
+    await browser.composeAction?.setIcon(details);
   } else {
     const details = {
       path: {
@@ -157,8 +189,8 @@ async function setIcon(iconFilename: string, tabId?: number): Promise<void> {
         48: `images/${iconFilename}-48.png`,
       },
     };
-    void browser.browserAction.setIcon({ ...details, tabId });
-    void browser.composeAction?.setIcon(details);
+    await browser.browserAction.setIcon({ ...details, tabId });
+    await browser.composeAction?.setIcon(details);
   }
 }
 
