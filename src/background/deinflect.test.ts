f2151016be489461cd91fe863d5b6c016e977c52
Brian Birtles <birtles@gmail.com>
2022-11-22 14:23:09 +0900

    chore: separate out from and to types in deinflection code

--- src/background/deinflect.test.ts
+++ src/background/deinflect.test.ts
@@ -1,11 +1,11 @@
-import { deinflect, DeinflectReason, WordType } from './deinflect';
+import { deinflect, Reason, WordType } from './deinflect';
 
 describe('deinflect', () => {
   it('performs de-inflection', () => {
     const result = deinflect('走ります');
     const match = result.find((candidate) => candidate.word === '走る');
     expect(match).toEqual({
-      reasons: [[DeinflectReason.Polite]],
+      reasons: [[Reason.Polite]],
       type: 2,
       word: '走る',
     });
@@ -15,9 +15,7 @@ describe('deinflect', () => {
     const result = deinflect('踊りたくなかった');
     const match = result.find((candidate) => candidate.word === '踊る');
     expect(match).toEqual({
-      reasons: [
-        [DeinflectReason.Tai, DeinflectReason.Negative, DeinflectReason.Past],
-      ],
+      reasons: [[Reason.Tai, Reason.Negative, Reason.Past]],
       type: 2,
       word: '踊る',
     });
@@ -25,15 +23,15 @@ describe('deinflect', () => {
 
   it('deinflects kana variations', () => {
     const cases = [
-      ['走ります', '走る', [[DeinflectReason.Polite]], 2],
-      ['走りまス', '走る', [[DeinflectReason.Polite]], 2],
-      ['走りマス', '走る', [[DeinflectReason.Polite]], 2],
-      ['走リマス', '走る', [[DeinflectReason.Polite]], 2],
-      ['走リマす', '走る', [[DeinflectReason.Polite]], 2],
-      ['走った', '走る', [[DeinflectReason.Past]], 2],
-      ['走っタ', '走る', [[DeinflectReason.Past]], 2],
-      ['走ッタ', '走る', [[DeinflectReason.Past]], 2],
-      ['走ッた', '走る', [[DeinflectReason.Past]], 2],
+      ['走ります', '走る', [[Reason.Polite]], 2],
+      ['走りまス', '走る', [[Reason.Polite]], 2],
+      ['走りマス', '走る', [[Reason.Polite]], 2],
+      ['走リマス', '走る', [[Reason.Polite]], 2],
+      ['走リマす', '走る', [[Reason.Polite]], 2],
+      ['走った', '走る', [[Reason.Past]], 2],
+      ['走っタ', '走る', [[Reason.Past]], 2],
+      ['走ッタ', '走る', [[Reason.Past]], 2],
+      ['走ッた', '走る', [[Reason.Past]], 2],
     ];
 
     for (const [inflected, plain, reasons, type] of cases) {
@@ -47,7 +45,7 @@ describe('deinflect', () => {
     const result = deinflect('食べ');
     const match = result.find((candidate) => candidate.word === '食べる');
     expect(match).toEqual({
-      reasons: [[DeinflectReason.MasuStem]],
+      reasons: [[Reason.MasuStem]],
       type: 1,
       word: '食べる',
     });
@@ -72,7 +70,7 @@ describe('deinflect', () => {
       const result = deinflect(inflected as string);
       const match = result.find((candidate) => candidate.word == plain);
       expect(match).toEqual({
-        reasons: [[DeinflectReason.Negative]],
+        reasons: [[Reason.Negative]],
         type: type,
         word: plain,
       });
@@ -83,7 +81,7 @@ describe('deinflect', () => {
     const result = deinflect('食べられぬ');
     const match = result.find((candidate) => candidate.word === '食べる');
     expect(match).toEqual({
-      reasons: [[DeinflectReason.PotentialOrPassive, DeinflectReason.Negative]],
+      reasons: [[Reason.PotentialOrPassive, Reason.Negative]],
       type: 9,
       word: '食べる',
     });
@@ -93,7 +91,7 @@ describe('deinflect', () => {
     const result = deinflect('き');
     const match = result.find((candidate) => candidate.word === 'くる');
     expect(match).toEqual({
-      reasons: [[DeinflectReason.MasuStem]],
+      reasons: [[Reason.MasuStem]],
       type: 8,
       word: 'くる',
     });
@@ -103,7 +101,7 @@ describe('deinflect', () => {
     const result = deinflect('美しき');
     const match = result.find((candidate) => candidate.word === '美しい');
     expect(match).toEqual({
-      reasons: [[DeinflectReason.Ki]],
+      reasons: [[Reason.Ki]],
       type: WordType.IAdj,
       word: '美しい',
     });
@@ -113,7 +111,7 @@ describe('deinflect', () => {
     const result = deinflect('兼した');
     const match = result.find((candidate) => candidate.word === '兼す');
     expect(match).toEqual({
-      reasons: [[DeinflectReason.Past]],
+      reasons: [[Reason.Past]],
       type: 18,
       word: '兼す',
     });
@@ -121,16 +119,16 @@ describe('deinflect', () => {
 
   it('deinflects irregular forms of 行く', () => {
     const cases = [
-      ['行った', '行く', DeinflectReason.Past, 2],
-      ['行って', '行く', DeinflectReason.Te, 2],
-      ['行ったり', '行く', DeinflectReason.Tari, 2],
-      ['行ったら', '行く', DeinflectReason.Tara, 2],
-      ['いった', 'いく', DeinflectReason.Past, 2],
-      ['いって', 'いく', DeinflectReason.Te, 2],
-      ['いったり', 'いく', DeinflectReason.Tari, 2],
-      ['いったら', 'いく', DeinflectReason.Tara, 2],
-      ['逝った', '逝く', DeinflectReason.Past, 2],
-      ['往った', '往く', DeinflectReason.Past, 2],
+      ['行った', '行く', Reason.Past, 2],
+      ['行って', '行く', Reason.Te, 2],
+      ['行ったり', '行く', Reason.Tari, 2],
+      ['行ったら', '行く', Reason.Tara, 2],
+      ['いった', 'いく', Reason.Past, 2],
+      ['いって', 'いく', Reason.Te, 2],
+      ['いったり', 'いく', Reason.Tari, 2],
+      ['いったら', 'いく', Reason.Tara, 2],
+      ['逝った', '逝く', Reason.Past, 2],
+      ['往った', '往く', Reason.Past, 2],
     ];
 
     for (const [inflected, plain, reason, type] of cases) {
@@ -173,7 +171,7 @@ describe('deinflect', () => {
       const result = deinflect(inflected);
       const match = result.find((candidate) => candidate.word == plain);
       expect(match).toEqual({
-        reasons: [[DeinflectReason.Past]],
+        reasons: [[Reason.Past]],
         type: 2,
         word: plain,
       });
@@ -181,9 +179,7 @@ describe('deinflect', () => {
   });
 
   it('deinflects the continuous form', () => {
-    const cases: Array<
-      [string, string, number, Array<DeinflectReason> | undefined]
-    > = [
+    const cases: Array<[string, string, number, Array<Reason> | undefined]> = [
       // U-verbs
       ['戻っている', '戻る', 2, undefined],
       ['戻ってる', '戻る', 2, undefined],
@@ -214,49 +210,19 @@ describe('deinflect', () => {
       ['きている', 'くる', 8, undefined],
       ['きてる', 'くる', 8, undefined],
       // Combinations
-      [
-        '戻っています',
-        '戻る',
-        2,
-        [DeinflectReason.Continuous, DeinflectReason.Polite],
-      ],
-      [
-        '戻ってます',
-        '戻る',
-        2,
-        [DeinflectReason.Continuous, DeinflectReason.Polite],
-      ],
-      [
-        '戻っていない',
-        '戻る',
-        2,
-        [DeinflectReason.Continuous, DeinflectReason.Negative],
-      ],
-      [
-        '戻ってない',
-        '戻る',
-        2,
-        [DeinflectReason.Continuous, DeinflectReason.Negative],
-      ],
-      [
-        '戻っていた',
-        '戻る',
-        2,
-        [DeinflectReason.Continuous, DeinflectReason.Past],
-      ],
-      [
-        '戻ってた',
-        '戻る',
-        2,
-        [DeinflectReason.Continuous, DeinflectReason.Past],
-      ],
+      ['戻っています', '戻る', 2, [Reason.Continuous, Reason.Polite]],
+      ['戻ってます', '戻る', 2, [Reason.Continuous, Reason.Polite]],
+      ['戻っていない', '戻る', 2, [Reason.Continuous, Reason.Negative]],
+      ['戻ってない', '戻る', 2, [Reason.Continuous, Reason.Negative]],
+      ['戻っていた', '戻る', 2, [Reason.Continuous, Reason.Past]],
+      ['戻ってた', '戻る', 2, [Reason.Continuous, Reason.Past]],
     ];
 
     for (const [inflected, plain, type, reasons] of cases) {
       const result = deinflect(inflected);
       const match = result.find((candidate) => candidate.word == plain);
       expect(match).toEqual({
-        reasons: reasons ? [reasons] : [[DeinflectReason.Continuous]],
+        reasons: reasons ? [reasons] : [[Reason.Continuous]],
         type,
         word: plain,
       });
@@ -266,6 +232,6 @@ describe('deinflect', () => {
     const result = deinflect('食べて');
     const match = result.find((candidate) => candidate.word == '食べる');
     expect(match).toBeDefined();
-    expect(match!.reasons).not.toContainEqual([DeinflectReason.Continuous]);
+    expect(match!.reasons).not.toContainEqual([Reason.Continuous]);
   });
 });
