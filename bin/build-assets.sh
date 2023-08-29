bf62d5218dfe8a8963b51ed7d5891eaf9e6c7360
Brian Birtles <birtles@gmail.com>
2023-08-29 18:30:07 +0900

    chore: rewrite release process using release-it

---
+++ bin/build-assets.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Check that BUGSNAG_API_KEY is set
+if [ -n "$BUGSNAG_API_KEY" ]; then
+	echo "BUGSNAG_API_KEY set."
+else
+	echo "BUGSNAG_API_KEY is not set."
+	exit 1
+fi
+
+# Check if jq is installed
+if ! command -v jq &>/dev/null; then
+	echo "jq is not installed. Please install jq to use this script."
+	exit 1
+fi
+
+# Get the directory containing the script
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Get the parent directory of the script's directory
+parent_dir="$(dirname "$script_dir")"
+
+# Set the current working directory to the script's directory
+cd "$parent_dir"
+echo -e "Running from ${parent_dir}"
+
+# Check for package.json
+if [ ! -f "./package.json" ]; then
+	echo "package.json file not found"
+	cd -
+	exit 1
+fi
+
+# Read package version
+version=$(jq -r .version "./package.json")
+if [ "$version" = "" ]; then
+	echo "version not found"
+	exit 1
+fi
+
+echo -e "Building assets for version ${version}...\n"
+
+# Make sure the output directory is available
+mkdir -p release-assets
+
+# Firefox package
+RELEASE_BUILD=1 yarn package:firefox
+mv dist-firefox-package/10ten_japanese_reader_rikaichamp_-${version}.zip release-assets/10ten-ja-reader-${version}-firefox.zip
+
+# Chrome package
+RELEASE_BUILD=1 yarn package:chrome
+mv dist-chrome-package/10ten_japanese_reader_rikaichamp_-${version}.zip release-assets/10ten-ja-reader-${version}-chrome.zip
+
+# Edge package
+RELEASE_BUILD=1 yarn package:edge
+mv dist-edge-package/10ten_japanese_reader_rikaichamp_-${version}.zip release-assets/10ten-ja-reader-${version}-edge.zip
+
+# Thunderbird package
+RELEASE_BUILD=1 yarn package:thunderbird
+mv dist-thunderbird-package/10ten_japanese_reader_rikaichamp_-${version}.zip release-assets/10ten-ja-reader-${version}-thunderbird.zip
+
+# Source package
+RELEASE_BUILD=1 yarn zip-src
+mv dist-src/10ten-ja-reader-${version}-src.zip release-assets/
+
+# Copy raw source files too
+cp dist-firefox/10ten-ja-*.js release-assets/
+
+# Restore current working directory
+cd -
