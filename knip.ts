eb838063e8bd8c214404c71ceaa8f6f6ad8e5914
Brian Birtles <birtles@gmail.com>
2023-09-15 16:59:21 +0900

    chore: teach knip a couple more entrypoints

--- knip.ts
+++ knip.ts
@@ -2,17 +2,23 @@ import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
   entry: [
-    // Utility script used by update docs pages
+    // Utility script used by update docs pages.
     'docs/update/update.js',
+    // A utility we used to generate all the icon variations once that might yet
+    // be useful again some day.
+    'scripts/generate-icons.ts',
     // This file is conditionally included in some configurations in a way knip
     // can't be expected to follow. Hopefully we'll eventually remove this
     // quirk and use the same tab manager everywhere.
     'src/background/active-tab-manager.ts',
     // Included by popups.html until we set up a better way of doing component
-    // tests
+    // tests.
     'tests/html-tests.js',
     // Likewise for options.html
     'tests/options.js',
+    // This is referenced in xcode13/Shared (App)/Base.lproj/Main.html but knip
+    // doesn't know how to parse that.
+    'xcode13/Shared \\(App\\)/Resources/Script.js',
   ],
   ignore: [
     // knip's GitHub actions plugin doesn't know how to handle custom actions
