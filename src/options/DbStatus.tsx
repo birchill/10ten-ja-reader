440fc1e049676dc3064f4815dc308042cd897bf7
Brian Birtles <birtles@gmail.com>
2023-10-02 21:20:47 +0900

    wip: start filling out DbStatus component a little more

--- src/options/DbStatus.tsx
+++ src/options/DbStatus.tsx
@@ -1,3 +1,18 @@
+import browser from 'webextension-polyfill';
+
 export function DbStatus() {
-  return <div>DbStatus</div>;
+  // XXX Need to linkify this
+  const attribution = browser.i18n.getMessage('options_data_source');
+  // XXX Need to do license and accent attribution too
+
+  return (
+    <div class="section-content panel-section-db-summary">
+      <div class="db-summary-blurb">{attribution}</div>
+      <div class="db-summary-status"></div>
+      <div class="db-admin" style="display: none">
+        <span>Database testing features: </span>
+        <button id="deleteDatabase">Delete database</button>
+      </div>
+    </div>
+  );
 }
