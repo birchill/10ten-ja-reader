6d4a10b7265679f852740c33e030306ca9ba0a62
Brian Birtles <birtles@gmail.com>
2023-10-04 21:36:08 +0900

    chore: add Linkify component

--- src/options/DbStatus.tsx
+++ src/options/DbStatus.tsx
@@ -1,15 +1,33 @@
 import { useLocale } from '../common/i18n';
+import { Linkify } from './Linkify';
 
 export function DbStatus() {
   const { t } = useLocale();
 
-  // XXX Need to linkify this
   const attribution = t('options_data_source');
   // XXX Need to do license and accent attribution too
 
   return (
     <div class="section-content panel-section-db-summary">
-      <div class="db-summary-blurb">{attribution}</div>
+      <div class="db-summary-blurb">
+        <Linkify
+          text={attribution}
+          links={[
+            {
+              keyword: 'JMdict/EDICT',
+              href: 'https://www.edrdg.org/wiki/index.php/JMdict-EDICT_Dictionary_Project',
+            },
+            {
+              keyword: 'KANJIDIC',
+              href: 'https://www.edrdg.org/wiki/index.php/KANJIDIC_Project',
+            },
+            {
+              keyword: 'JMnedict/ENAMDICT',
+              href: 'https://www.edrdg.org/enamdict/enamdict_doc.html',
+            },
+          ]}
+        />
+      </div>
       <div class="db-summary-status"></div>
       <div class="db-admin" style="display: none">
         <span>Database testing features: </span>
---
+++ src/options/Linkify.tsx
@@ -0,0 +1,56 @@
+type LinkSpec = {
+  keyword: string;
+  href: string;
+};
+
+type Props = {
+  text: string;
+  links: Array<LinkSpec>;
+};
+
+export function Linkify(props: Props) {
+  const matchedReplacements: Array<{
+    index: number;
+    keyword: string;
+    href: string;
+  }> = [];
+
+  for (const link of props.links) {
+    const index = props.text.indexOf(link.keyword);
+    if (index !== -1) {
+      matchedReplacements.push({ index, ...link });
+    }
+  }
+  matchedReplacements.sort((a, b) => a.index - b.index);
+
+  let position = 0;
+  const parts: Array<string | LinkSpec> = [];
+
+  for (const replacement of matchedReplacements) {
+    if (position < replacement.index) {
+      parts.push(props.text.substring(position, replacement.index));
+    }
+
+    parts.push({ href: replacement.href, keyword: replacement.keyword });
+
+    position = replacement.index + replacement.keyword.length;
+  }
+
+  if (position < props.text.length) {
+    parts.push(props.text.substring(position, props.text.length));
+  }
+
+  return (
+    <>
+      {parts.map((part) =>
+        typeof part === 'string' ? (
+          part
+        ) : (
+          <a href={part.href} target="_blank" rel="noopener">
+            {part.keyword}
+          </a>
+        )
+      )}
+    </>
+  );
+}
