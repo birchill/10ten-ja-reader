04f333a1355361ee16b0d446b3eb76de98b005c6
Brian Birtles <birtles@gmail.com>
2023-10-05 21:55:00 +0900

    feat: fill out offline database state

--- src/options/DbStatus.fixture.tsx
+++ src/options/DbStatus.fixture.tsx
@@ -1,5 +1,34 @@
 import { DbStatus } from './DbStatus';
 
 export default {
-  default: () => <DbStatus />,
+  offline: () => (
+    <DbStatus
+      dbState={{
+        words: {
+          state: 'empty',
+          version: null,
+        },
+        kanji: {
+          state: 'empty',
+          version: null,
+        },
+        radicals: {
+          state: 'empty',
+          version: null,
+        },
+        names: {
+          state: 'empty',
+          version: null,
+        },
+        updateState: {
+          type: 'idle',
+          lastCheck: null,
+        },
+        updateError: {
+          name: 'OfflineError',
+          message: 'Offline',
+        },
+      }}
+    />
+  ),
 };
--- src/options/DbStatus.tsx
+++ src/options/DbStatus.tsx
@@ -1,7 +1,25 @@
 import { useLocale } from '../common/i18n';
 import { Linkify } from './Linkify';
+import { JpdictState } from '../background/jpdict';
 
-export function DbStatus() {
+type Props = {
+  dbState: JpdictState;
+};
+
+export function DbStatus(props: Props) {
+  return (
+    <div class="section-content panel-section-db-summary">
+      <DbSummaryBlurb />
+      <DbSummaryStatus dbState={props.dbState} />
+      <div class="db-admin" hidden>
+        <span>Database testing features: </span>
+        <button id="deleteDatabase">Delete database</button>
+      </div>
+    </div>
+  );
+}
+
+function DbSummaryBlurb() {
   const { t } = useLocale();
 
   const attribution = t('options_data_source');
@@ -10,45 +28,65 @@ export function DbStatus() {
   const accentAttribution = t('options_accent_data_source');
 
   return (
-    <div class="section-content panel-section-db-summary">
-      <div class="db-summary-blurb">
-        <Linkify
-          text={attribution}
-          links={[
-            {
-              keyword: 'JMdict/EDICT',
-              href: 'https://www.edrdg.org/wiki/index.php/JMdict-EDICT_Dictionary_Project',
-            },
-            {
-              keyword: 'KANJIDIC',
-              href: 'https://www.edrdg.org/wiki/index.php/KANJIDIC_Project',
-            },
-            {
-              keyword: 'JMnedict/ENAMDICT',
-              href: 'https://www.edrdg.org/enamdict/enamdict_doc.html',
-            },
-          ]}
-        />
-        <Linkify
-          text={license}
-          links={[
-            {
-              keyword: 'Electronic Dictionary Research and Development Group',
-              href: 'https://www.edrdg.org/',
-            },
-            {
-              keyword: licenseKeyword,
-              href: 'https://www.edrdg.org/edrdg/licence.html',
-            },
-          ]}
-        />
-        <p>{accentAttribution}</p>
-      </div>
-      <div class="db-summary-status"></div>
-      <div class="db-admin" style="display: none">
-        <span>Database testing features: </span>
-        <button id="deleteDatabase">Delete database</button>
-      </div>
+    <div class="db-summary-blurb">
+      <Linkify
+        text={attribution}
+        links={[
+          {
+            keyword: 'JMdict/EDICT',
+            href: 'https://www.edrdg.org/wiki/index.php/JMdict-EDICT_Dictionary_Project',
+          },
+          {
+            keyword: 'KANJIDIC',
+            href: 'https://www.edrdg.org/wiki/index.php/KANJIDIC_Project',
+          },
+          {
+            keyword: 'JMnedict/ENAMDICT',
+            href: 'https://www.edrdg.org/enamdict/enamdict_doc.html',
+          },
+        ]}
+      />
+      <Linkify
+        text={license}
+        links={[
+          {
+            keyword: 'Electronic Dictionary Research and Development Group',
+            href: 'https://www.edrdg.org/',
+          },
+          {
+            keyword: licenseKeyword,
+            href: 'https://www.edrdg.org/edrdg/licence.html',
+          },
+        ]}
+      />
+      <p>{accentAttribution}</p>
     </div>
   );
+}
+
+function DbSummaryStatus(props: { dbState: JpdictState }) {
+  if (props.dbState.updateState.type === 'idle') {
+    return <IdleStateSummary dbState={props.dbState} />;
+  }
+
+  // TODO Other cases
+
+  return <div class="db-summary-status"></div>;
+}
+
+function IdleStateSummary(props: { dbState: JpdictState }) {
+  const { t } = useLocale();
+  const { updateError } = props.dbState;
+
+  if (!!updateError && updateError.name === 'OfflineError') {
+    return (
+      <div class="db-summary-status -warning">
+        <div class="db-summary-info">{t('options_offline_explanation')}</div>
+      </div>
+    );
+  }
+
+  // TODO Other cases
+
+  return <div class="db-summary-status"></div>;
 }
