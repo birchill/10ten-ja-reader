af2a629bf4ba2de3a6671deb49dd1d9a7f4170aa
Brian Birtles <birtles@gmail.com>
2023-10-17 13:27:40 +0900

    chore: tweak eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,74 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:tailwindcss/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.mjs', 'tests/**/*', 'utils/**/*'],
+      parserOptions: {
+        project: null,
+        sourceType: 'module',
+      },
+      rules: {
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/no-misused-promises': 'off',
+      },
+    },
+  ],
+  plugins: ['@typescript-eslint', 'vitest'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'no-constant-condition': ['error', { checkLoops: false }],
+    'no-prototype-builtins': 'off',
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all',
+      },
+    ],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    semi: ['error', 'always'],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        allowSeparatedGroups: true,
+      },
+    ],
+    'tailwindcss/classnames-order': 'off',
+    'tailwindcss/no-custom-classname': 'off',
+    'vitest/no-identical-title': ['error'],
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-floating-promises': ['warn', { ignoreIIFE: false }],
+    '@typescript-eslint/no-misused-promises': [
+      'warn',
+      { checksVoidReturn: false },
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/triple-slash-reference': [
+      'error',
+      {
+        path: 'always',
+        types: 'never',
+        lib: 'never',
+      },
+    ],
+  },
+};
