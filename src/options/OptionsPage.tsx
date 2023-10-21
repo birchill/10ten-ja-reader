78f83b9fcca8b6dcf1550099866e68a644da9293
Brian Birtles <birtles@gmail.com>
2023-10-21 11:53:23 +0900

    chore: move SectionHeading to a separate file

--- src/options/OptionsPage.tsx
+++ src/options/OptionsPage.tsx
@@ -1,14 +1,13 @@
-import type { RenderableProps } from 'preact';
 import { useCallback } from 'preact/hooks';
 
 import type { Config } from '../common/config';
 import { I18nProvider, useLocale } from '../common/i18n';
 import { ReferenceAbbreviation } from '../common/refs';
 import { getReleaseStage } from '../utils/release-stage';
-import type { EmptyProps } from '../utils/type-helpers';
 
 import { DbStatus } from './DbStatus';
 import { KanjiReferenceSetting } from './KanjiReferenceSetting';
+import { SectionHeading } from './SectionHeading';
 import { useDb } from './use-db';
 import { useConfigValue } from './use-config-value';
 
@@ -68,32 +67,5 @@ function OptionsPageInner(props: Props) {
         onUpdateDb={startDatabaseUpdate}
       />
     </>
-  );
-}
-
-function SectionHeading(props: RenderableProps<EmptyProps>) {
-  // TODO: Once we converted all headings to Tailwind, we need to apply the
-  // special styling to the first heading in the section to remove its border
-  // and padding, and reduce the margin.
-  //
-  // For reference, the styles we use(d) for this are:
-  //
-  //  .section-header {
-  //    font-size: 1.46em;
-  //    font-weight: 300;
-  //    line-height: 1.3em;
-  //    margin: 8px 0;
-  //  }
-  //
-  //  .section-header:nth-of-type(n + 2) {
-  //    margin-top: 16px;
-  //    padding-top: 16px;
-  //    border-top: 1px solid lightgrey;
-  //  }
-  //
-  return (
-    <h1 class="mt-4 border-0 border-t border-solid border-t-gray-300 pt-4 text-2xl font-light">
-      {props.children}
-    </h1>
   );
 }
