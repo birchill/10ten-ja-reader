8d61624c9f8628b643513ec8874ceb080bf2efa0
Brian Birtles <birtles@gmail.com>
2024-02-02 10:55:28 +0900

    chore: drop eslint-config-prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:tailwindcss/recommended',
-    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
