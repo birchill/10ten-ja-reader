e0db6d511d18bc0fb3322d964830fc842e6a95d5
Brian Birtles <birtles@gmail.com>
2024-05-11 13:07:56 +0900

    chore: move popup positioning logic into showPopup

--- src/content/popup/render-popup.ts
+++ src/content/popup/render-popup.ts
@@ -7,10 +7,7 @@ import { html } from '../../utils/builder';
 import { Point } from '../../utils/geometry';
 import { getThemeClass } from '../../utils/themes';
 
-import {
-  getOrCreateEmptyContainer,
-  removeContentContainer,
-} from '../content-container';
+import { getOrCreateEmptyContainer } from '../content-container';
 import { DisplayMode } from '../popup-state';
 import { LookupPuck } from '../puck';
 import { QueryResult } from '../query';
@@ -30,6 +27,7 @@ import { renderTabBar } from './tabs';
 import { renderWordEntries } from './words';
 
 import popupStyles from '../../../css/popup.css';
+import { getPopupContainer } from './popup-container';
 
 export function renderPopup(
   result: QueryResult | undefined,
@@ -340,87 +338,6 @@ function resetContainer({
   return windowDiv;
 }
 
-export function isPopupVisible(): boolean {
-  const popupContainer = getPopupContainer();
-  return !!popupContainer && !popupContainer.classList.contains('hidden');
-}
-
-export function hidePopup() {
-  getPopupContainer()?.classList.add('hidden');
-}
-
-function getPopupContainer(): HTMLElement | null {
-  const hostElem = document.getElementById('tenten-ja-window');
-  return hostElem && hostElem.shadowRoot
-    ? hostElem.shadowRoot.querySelector('.container')
-    : null;
-}
-
-export function removePopup() {
-  removeContentContainer(['rikaichamp-window', 'tenten-ja-window']);
-  document.getElementById('tenten-doc-styles')?.remove();
-}
-
-export function setFontSize(size: FontSize) {
-  const popupWindow = getPopupWindow();
-  if (!popupWindow) {
-    return;
-  }
-
-  for (const className of popupWindow.classList.values()) {
-    if (className.startsWith('font-')) {
-      popupWindow.classList.remove(className);
-    }
-  }
-
-  if (size !== 'normal') {
-    popupWindow.classList.add(`font-${size}`);
-  }
-}
-
-export function setPopupStyle(style: string) {
-  const elems = [getPopupWindow(), getPopupArrow()];
-
-  for (const elem of elems) {
-    if (!elem) {
-      continue;
-    }
-
-    for (const className of elem.classList.values()) {
-      if (className.startsWith('theme-')) {
-        elem.classList.remove(className);
-      }
-    }
-
-    elem.classList.add(getThemeClass(style));
-  }
-}
-
-function getPopupWindow(): HTMLElement | null {
-  const hostElem = document.getElementById('tenten-ja-window');
-  return hostElem && hostElem.shadowRoot
-    ? hostElem.shadowRoot.querySelector('.window')
-    : null;
-}
-
-export function getPopupDimensions(hostElem: HTMLElement): {
-  width: number;
-  height: number;
-} {
-  // Measure the size of the inner window so that we don't include the padding
-  // for the shadow
-  const windowElem = hostElem.shadowRoot?.querySelector('.window');
-  const width =
-    (windowElem instanceof HTMLElement ? windowElem.offsetWidth : 0) || 200;
-  const height =
-    windowElem instanceof HTMLElement ? windowElem.offsetHeight : 0;
-  return { width, height };
-}
-
-export function isPopupWindowHostElem(target: EventTarget | null): boolean {
-  return target instanceof HTMLElement && target.id === 'tenten-ja-window';
-}
-
 function showOverlay(copyState: CopyState): boolean {
   return (
     (copyState.kind === 'active' || copyState.kind === 'error') &&
@@ -458,11 +375,4 @@ export function renderPopupArrow(options: {
   }
 
   renderArrow({ ...options, popupContainer, target });
-}
-
-function getPopupArrow(): HTMLElement | null {
-  const hostElem = document.getElementById('tenten-ja-window');
-  return hostElem && hostElem.shadowRoot
-    ? hostElem.shadowRoot.querySelector('.arrow')
-    : null;
 }
