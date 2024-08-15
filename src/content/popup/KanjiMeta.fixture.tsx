2c5639c3cfc8dbdefdf4b6ad1c24d264e575b3fe
Brian Birtles <birtles@gmail.com>
2024-08-15 13:18:51 +0900

    chore: add KanjiMeta component

---
+++ src/content/popup/KanjiMeta.fixture.tsx
@@ -0,0 +1,5 @@
+import { KanjiMeta } from './KanjiMeta';
+
+export default {
+  default: <KanjiMeta tags={['kokuji', 'phantom kanji']} />,
+};
---
+++ src/content/popup/KanjiMeta.tsx
@@ -0,0 +1,21 @@
+import { useLocale } from '../../common/i18n';
+
+export type Props = { tags: Array<string> };
+
+export function KanjiMeta(props: Props) {
+  const { t, langTag } = useLocale();
+
+  return (
+    <div class="tp-flex tp-gap-2 -tp-mx-1">
+      {props.tags.map((tag) => (
+        <span
+          key={tag}
+          class="tp-text-sm tp-border tp-border-current tp-border-solid tp-rounded tp-py-0.5 tp-px-1"
+          lang={langTag}
+        >
+          {t(`content_kanji_meta_${tag.replace(' ', '_')}`)}
+        </span>
+      ))}
+    </div>
+  );
+}
