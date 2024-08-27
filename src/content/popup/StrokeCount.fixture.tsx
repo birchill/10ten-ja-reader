2e2e51c195f0e510e82d2037a4256e42331cc286
Brian Birtles <birtles@gmail.com>
2024-08-27 13:23:14 +0900

    chore: add play state to StrokeCount component

--- src/content/popup/StrokeCount.fixture.tsx
+++ src/content/popup/StrokeCount.fixture.tsx
@@ -1,25 +1,40 @@
+import { useState } from 'preact/hooks';
+
 import { StrokeCount } from './StrokeCount';
 
 export default {
-  default: (
-    <div
-      style={{
-        background: 'var(--bg-color)',
-        color: 'var(--text-color)',
-        padding: '18px',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '40px',
-      }}
-    >
-      <div>
-        <h2>Singular</h2>
-        <StrokeCount sc={1} />
+  default: () => {
+    const [paused, setPaused] = useState(true);
+    const togglePaused = () => setPaused((prev) => !prev);
+
+    return (
+      <div
+        style={{
+          background: 'var(--bg-color)',
+          color: 'var(--text-color)',
+          padding: '18px',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '40px',
+        }}
+      >
+        <div>
+          <h2>Singular</h2>
+          <StrokeCount sc={1} />
+        </div>
+        <div>
+          <h2>Plural</h2>
+          <StrokeCount sc={17} />
+        </div>
+        <div>
+          <h2>With animation</h2>
+          <StrokeCount
+            sc={11}
+            onToggleAnimation={togglePaused}
+            isPlaying={!paused}
+          />
+        </div>
       </div>
-      <div>
-        <h2>Plural</h2>
-        <StrokeCount sc={17} />
-      </div>
-    </div>
-  ),
+    );
+  },
 };
--- src/content/popup/StrokeCount.tsx
+++ src/content/popup/StrokeCount.tsx
@@ -1,6 +1,11 @@
 import { useLocale } from '../../common/i18n';
+import { classes } from '../../utils/classes';
 
-export type Props = { sc: number };
+export type Props = {
+  sc: number;
+  isPlaying?: boolean;
+  onToggleAnimation?: () => void;
+};
 
 export function StrokeCount(props: Props) {
   const { t } = useLocale();
@@ -11,7 +16,13 @@ export function StrokeCount(props: Props) {
       : t('content_kanji_strokes_label', [String(props.sc)]);
 
   return (
-    <div class="tp-flex tp-gap-1.5 tp-items-center tp-text-smish">
+    <div
+      class={classes(
+        'tp-flex tp-gap-1.5 tp-items-center tp-text-smish',
+        props.onToggleAnimation &&
+          'tp-cursor-pointer tp-text-[--cell-link-fg] tp-underline tp-decoration-dotted tp-underline-offset-2'
+      )}
+    >
       <svg
         class="tp-block tp-h-[12px] tp-w-[12px] tp-fill-current tp-opacity-50"
         role="presentation"
@@ -28,6 +39,20 @@ export function StrokeCount(props: Props) {
         />
       </svg>
       <span>{strokeLabel}</span>
+      {props.onToggleAnimation && (
+        <svg
+          class="tp-block tp-h-[12px] tp-w-[12px] tp-fill-current tp-opacity-50"
+          role="presentation"
+          viewBox="0 0 24 24"
+          onClick={props.onToggleAnimation}
+        >
+          {props.isPlaying ? (
+            <rect width="24" height="24" rx="4" ry="4" />
+          ) : (
+            <path d="M24 12c0 .4-.1.8-.32 1.15-.21.35-.51.64-.88.84L3.52 23.74c-.34.17-.73.26-1.12.26-.43 0-.86-.12-1.23-.34a2.3 2.3 0 01-.86-.83C.1 22.5 0 22.1 0 21.7V2.3c0-.4.11-.79.32-1.13A2.43 2.43 0 012.33 0c.42-.01.83.08 1.2.26l19.24 9.73c.01 0 .01.02.03.02.37.2.67.49.88.84.21.35.32.74.32 1.15z" />
+          )}
+        </svg>
+      )}
     </div>
   );
 }
