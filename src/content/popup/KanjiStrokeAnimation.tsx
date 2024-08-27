6529198d88285ba7fb3de481f152d987b6a3473d
Brian Birtles <birtles@gmail.com>
2024-08-27 12:22:49 +0900

    feat: make it possible to seek by clicking

--- src/content/popup/KanjiStrokeAnimation.tsx
+++ src/content/popup/KanjiStrokeAnimation.tsx
@@ -14,22 +14,22 @@ const STROKE_SPEED = 150; // User units / second
 const STROKE_GAP = 250; // ms
 const FREEZE_LENGTH = 1000; // ms
 
-// This is deliberately larger than the actual range the scrubber dot moves
-// (80px) so that you have a bit more control over seeking.
-const SCRUBBER_RANGE = 100; // px in SVG user unit space
+const SCRUBBER_RANGE = 80; // px in SVG user unit space
+// This is deliberately larger than the actual range the scrubber moves
+// so that you have a bit more control over seeking.
+const SCRUBBER_DRAG_RANGE = 100; // px in SVG user unit space
+const TIMELINE_PADDING = 10; // px in SVG user unit space
 
 export function KanjiStrokeAnimation(props: Props) {
   const animatedStrokeContainer = useRef<SVGGElement>(null);
-  const scrubberSvg = useRef<SVGSVGElement>(null);
+  const timelineSvg = useRef<SVGSVGElement>(null);
   const scrubberContainer = useRef<SVGGElement>(null);
   const subpaths = useMemo(() => props.st.split(/(?=M[0-9])/), [props.st]);
 
   const currentAnimations = useRef<Array<Animation>>([]);
 
-  const { onScrubberPointerDown, applySeek } = useDraggableScrubber(
-    scrubberSvg,
-    currentAnimations
-  );
+  const { applySeek, onScrubberPointerDown, onTimelineClick } =
+    useDraggableScrubber(timelineSvg, currentAnimations);
 
   // Update the animation parameters
   useLayoutEffect(() => {
@@ -132,7 +132,12 @@ export function KanjiStrokeAnimation(props: Props) {
         </g>
       </svg>
       <div>
-        <svg class="tp-w-big-kanji" viewBox="0 0 100 20" ref={scrubberSvg}>
+        <svg
+          class="tp-w-big-kanji"
+          viewBox="0 0 100 20"
+          ref={timelineSvg}
+          onClick={onTimelineClick}
+        >
           <rect
             width={100}
             height={20}
@@ -153,7 +158,7 @@ export function KanjiStrokeAnimation(props: Props) {
               onPointerDown={onScrubberPointerDown}
             />
             <circle
-              cx={10}
+              cx={TIMELINE_PADDING}
               cy={10}
               r={6}
               class="tp-fill-[--primary-highlight] tp-opacity-50 peer-hover:tp-opacity-100"
@@ -167,11 +172,12 @@ export function KanjiStrokeAnimation(props: Props) {
 }
 
 function useDraggableScrubber(
-  scrubberSvg: RefObject<SVGSVGElement>,
+  timelineSvg: RefObject<SVGSVGElement>,
   currentAnimations: MutableRef<Array<Animation>>
 ): {
-  onScrubberPointerDown: (event: PointerEvent) => void;
   applySeek: (animations: Array<Animation>) => void;
+  onScrubberPointerDown: (event: PointerEvent) => void;
+  onTimelineClick: (event: MouseEvent) => void;
 } {
   const seekState = useRef<{ scrubberStart: number; offset: number } | null>(
     null
@@ -199,15 +205,18 @@ function useDraggableScrubber(
   // The following callbacks need to be stable so we can unregister them from
   // the window when dragging stops or the component unmounts.
 
-  const onPointerMove = useCallback((event: PointerEvent) => {
-    if (!seekState.current || !scrubberSvg.current) {
+  const onWindowPointerMove = useCallback((event: PointerEvent) => {
+    if (!seekState.current || !timelineSvg.current) {
       return;
     }
 
     // Calculate the offset of the scrubber
-    const [svgX] = toSvgCoords(scrubberSvg.current, event.clientX, 0);
+    const [svgX] = toSvgCoords(timelineSvg.current, event.clientX, 0);
     const offset = Math.min(
-      Math.max((svgX - seekState.current.scrubberStart) / SCRUBBER_RANGE, 0),
+      Math.max(
+        (svgX - seekState.current.scrubberStart) / SCRUBBER_DRAG_RANGE,
+        0
+      ),
       1
     );
     seekState.current.offset = offset;
@@ -216,7 +225,7 @@ function useDraggableScrubber(
     applySeek(currentAnimations.current);
   }, []);
 
-  const onPointerUpOrCancel = useCallback(() => {
+  const onWindowPointerUpOrCancel = useCallback(() => {
     if (!seekState.current) {
       return;
     }
@@ -224,23 +233,23 @@ function useDraggableScrubber(
     currentAnimations.current.forEach((animation) => animation.play());
     seekState.current = null;
 
-    window.removeEventListener('pointermove', onPointerMove);
-    window.removeEventListener('pointerup', onPointerUpOrCancel);
-    window.removeEventListener('pointercancel', onPointerUpOrCancel);
+    window.removeEventListener('pointermove', onWindowPointerMove);
+    window.removeEventListener('pointerup', onWindowPointerUpOrCancel);
+    window.removeEventListener('pointercancel', onWindowPointerUpOrCancel);
   }, []);
 
   useEffect(() => {
     return () => {
       if (seekState.current) {
-        window.removeEventListener('pointermove', onPointerMove);
-        window.removeEventListener('pointerup', onPointerUpOrCancel);
-        window.removeEventListener('pointercancel', onPointerUpOrCancel);
+        window.removeEventListener('pointermove', onWindowPointerMove);
+        window.removeEventListener('pointerup', onWindowPointerUpOrCancel);
+        window.removeEventListener('pointercancel', onWindowPointerUpOrCancel);
       }
     };
   }, []);
 
-  const onPointerDown = (event: PointerEvent) => {
-    if (seekState.current || !scrubberSvg.current) {
+  const onScrubberPointerDown = (event: PointerEvent) => {
+    if (seekState.current || !timelineSvg.current) {
       return;
     }
 
@@ -259,20 +268,43 @@ function useDraggableScrubber(
     );
 
     // Based on that, work out where the scrubber should start
-    const [svgX] = toSvgCoords(scrubberSvg.current, event.clientX, 0);
-    const scrubberStart = svgX - strokeAnimationProgress * SCRUBBER_RANGE;
+    const [svgX] = toSvgCoords(timelineSvg.current, event.clientX, 0);
+    const scrubberStart = svgX - strokeAnimationProgress * SCRUBBER_DRAG_RANGE;
     seekState.current = { scrubberStart, offset: strokeAnimationProgress };
 
     // Pause the animations
     currentAnimations.current.forEach((animation) => animation.pause());
 
     // Register the move/up/cancel events
-    window.addEventListener('pointermove', onPointerMove);
-    window.addEventListener('pointerup', onPointerUpOrCancel);
-    window.addEventListener('pointercancel', onPointerUpOrCancel);
+    window.addEventListener('pointermove', onWindowPointerMove);
+    window.addEventListener('pointerup', onWindowPointerUpOrCancel);
+    window.addEventListener('pointercancel', onWindowPointerUpOrCancel);
   };
 
-  return { onScrubberPointerDown: onPointerDown, applySeek };
+  const onTimelineClick = useCallback((event: MouseEvent) => {
+    if (seekState.current || !timelineSvg.current) {
+      return;
+    }
+
+    const [svgX] = toSvgCoords(timelineSvg.current, event.clientX, 0);
+    const offset = Math.min(
+      Math.max((svgX - TIMELINE_PADDING) / SCRUBBER_RANGE, 0),
+      1
+    );
+
+    // Seek the animations to that point
+    for (const animation of currentAnimations.current) {
+      const timing = animation.effect!.getComputedTiming();
+      animation.currentTime =
+        offset * ((timing.duration as number) - FREEZE_LENGTH);
+    }
+  }, []);
+
+  return {
+    onScrubberPointerDown,
+    onTimelineClick,
+    applySeek,
+  };
 }
 
 function toSvgCoords(
