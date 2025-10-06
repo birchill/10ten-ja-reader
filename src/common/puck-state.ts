c3c234e359d700833f04d19620a5bc1d91ccb616
Jack Arrington <mail@jackarrington.com>
2025-10-06 08:27:28 +0700

    feat: Add vertical reading mode to mobile lookup puck (#2557)
    - Puck can now be toggled between horizontal/vertical with a long press.
    - Added optional 'handedness' setting that adds extra spacing between
      moon and earth on side of users hand
    - Added an animation that displays a double sided arrow that rotates
      when the user switches reading directions
    - Added instructions and animation for the long press to the iOS app.

--- src/common/puck-state.ts
+++ src/common/puck-state.ts
@@ -1,6 +1,13 @@
 export type PuckState = {
   x: number;
   y: number;
-  orientation: 'above' | 'below';
+  /**
+   * Orientation of the moon. When reading direction is horizontal, can
+   * be switched between above and below the earth; when horizontal, can
+   * be switched between left and right sides of the earth.
+   */
+  orientation:
+    | { readingDirection: 'horizontal'; moonSide: 'above' | 'below' }
+    | { readingDirection: 'vertical'; moonSide: 'left' | 'right' };
   active: boolean;
 };
