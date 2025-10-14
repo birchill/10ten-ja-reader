ac2e078796d2bfc25ee707a43a8156d23d9efca3
Brian Birtles <birtles@gmail.com>
2025-10-14 20:10:55 +0900

    feat: auto-expand popup when looking up title fields

--- src/content/popup/render-popup.ts
+++ src/content/popup/render-popup.ts
@@ -251,6 +251,7 @@ export function renderPopup(
   )) {
     updateExpandable(expandable, {
       ...options,
+      isExpanded: options.isExpanded || !!result?.title,
       showKeyboardShortcut: options.displayMode === 'static',
     });
   }
