a9417ec56a051bb4bc5c1198f39e7dbdb84f3f07
Brian Birtles <birtles@gmail.com>
2025-11-05 10:52:24 +0900

    chore: drop no-longer-needed vitest-github-actions-reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,3 @@
-import GithubActionsReporter from 'vitest-github-actions-reporter';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
@@ -7,9 +6,6 @@ export default defineConfig({
       './{scripts,src}/**/*.test.{js,mjs,cjs,ts,mts,cts}',
       '.github/actions/**/*.test.{js,mjs,cjs,ts,mts,cts}',
     ],
-    reporters: process.env.GITHUB_ACTIONS
-      ? ['default', new GithubActionsReporter()]
-      : 'default',
     setupFiles: ['fake-indexeddb/auto'],
   },
 });
